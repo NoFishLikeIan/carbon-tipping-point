09265a9b32ee5e3ba4b36bdc0fcdc4d5bedd1cab
NoFishLikeIan <andreatitton96@gmail.com>
2023-04-19 16:56:38 +0200

    🧹 cleanup code

--- statecostate.jl
+++ statecostate.jl
@@ -1,4 +1,6 @@
-using DifferentialEquations, DynamicalSystems
+using DifferentialEquations
+using DynamicalSystems
+
 using Roots
 using LinearAlgebra
 
@@ -12,104 +14,37 @@ include("utils/dynamicalsystems.jl")
 include("model/climate.jl")
 include("model/economic.jl")
 
+include("statecostate/optimalpollution.jl")
 
-c₀ = 410 # Current carbon concentration
-x₀ = first(φ⁻¹(c₀, m)) # There are three solutions to φ⁻¹(c₀), we are in the low stable temperature equilibrium
-
-xₛ = first(φ⁻¹(m.cₚ, m)) # Surely safe temperature
 
 # State costate dynamics
 m = MendezFarazmand() # Climate model
-l = LinearQuadratic(τ = 0, γ = 0.15, xₛ = xₛ) # Social planner
+c₀ = 410 # Current carbon concentration
+x₀ = first(φ⁻¹(c₀, m)) # Current temperature
+xₛ = first(φ⁻¹(m.cₚ, m)) # Surely safe temperature
 eᵤ = (l.β₀ - l.τ) / l.β₁ # Unconstrained emissions
 
+l = LinearQuadratic(τ = 0, γ = 100., xₛ = xₛ) # Social planner
 xₗ, xᵤ = l.xₛ, 1.5l.xₛ # Bounds on temperature
-
-f(x, c, λ, e) = F!(zeros(4), [x, c, λ, e], [m, l], 0.)
-function F!(dz, z, p, t)
-	m, l = p # Unpack a LinearQuadratic model
-	x, c, λ, e = z # Unpack state
-	
-	dz[1] = m.κ * μ(x, c, m) # Temperature 
-	dz[2] = e - m.δ * c # Concentration 
-
-	dz[3] = (l.ρ - m.κ * ∂xμ(x, m)) * λ + l.γ * (x - l.xₛ) # Shadow price of temperature
-
-	dz[4] = (l.ρ + m.δ) * e - m.κ * (m.A / l.β₁) * (λ / c) - eᵤ # Emission dynamics
-
-	return dz
-end
-
-function DF!(D, z, p, t)
-	m, l = p # Unpack a LinearQuadratic model
-	x, c, λ, e = z # Unpack state
-
-	J = zeros(4, 4)
-
-	J[1, 1] = m.κ * ∂xμ(x, m)
-	J[1, 2] = m.κ * m.A / c
-
-	J[2, 2] = -m.δ
-	J[2, 4] = 1
-	
-	J[3, 1] = l.γ - m.κ * ∂xxμ(x, m) * λ
-	J[3, 3] = - m.κ * ∂xμ(x, m)
-
-	J[4, 2] = -m.κ * (m.A / l.β₁) * (λ / c^2)
-	J[4, 3] = -m.κ * (m.A / l.β₁) * (1 / c)
-	J[4, 4] = l.ρ + m.δ
-
-	D .= J
-
-	return J
-end
-
-# Equilibrium relations assuming e = δc
-
-"""
-Get the equilibria of the deterministic climate-economy model.
-"""
-function getequilibria(m::MendezFarazmand, l::LinearQuadratic)
-	ψ(x) = φ(x, m) * m.δ
-	ω(x) = l.γ * (x - l.xₛ) / (m.κ * ∂xμ(x, m) - l.ρ)
-	ϕ(e) = l.β₁ * e * ((l.ρ + m.δ) * e - eᵤ) / (m.δ * m.κ * m.A)
-	
-	equilibriumcond(x) = ω(x) - (ϕ ∘ ψ)(x)
-	
-	asymptotesω = find_zeros(x -> m.κ * ∂xμ(x, m) - l.ρ, xₗ, xᵤ)
-	
-	regions = [
-		(xₗ, asymptotesω[1]),
-		(asymptotesω[1], asymptotesω[2]),
-		(asymptotesω[2], xᵤ)
-	]
-	
-	
-	equilibria = Vector{Float64}[]
-	for (l, u) ∈ regions, x ∈ find_zeros(equilibriumcond, l, u)
-		e = ψ(x)
-		λ = ω(x)
-		c = e / m.δ
-	
-		push!(equilibria, [x, c, λ, e])
-	end
-	equilibria
-end
 
 equilibria = getequilibria(m, l)
 
-manifolds = computemanifolds(
-	F!, DF!, equilibria, [m, l];
-	dt = 0.01 	
-)
+solver = (alg = TanYam7(), abstol = 1.0e-8, reltol = 1.0e-8, dt = 1e-2)
 
 # Vector field plot
 λnull, enull = equilibria[1][3:4]
 function g(c, x) 
-	dx, dc, dλ, de = f(x, c, λnull, enull)
+	dx, dc, dλ, de = F([x, c, λnull, enull])
 	
 	return [dc, dx]
 end
+	
+u₀ = [x₀, c₀, λnull, enull]
+ds = TangentDynamicalSystem(
+	CoupledODEs(F!, u₀, [m, l]; solver...),
+	J = DF!, J0 = DF(u₀)
+)
+
 begin
 	celsiustokelvin = 273.15
 	narrows = 17
@@ -129,6 +64,10 @@ begin
 
 	for (i, z) ∈ enumerate(equilibria)
 		x, c, λ, e = z
+		
+		J = DF!(zeros(4, 4), z, [m, l], 0.)
+		λ, v = eigen(J)
+
 		scatter!(vecfig, [c], [x]; 
 			label = i == 1 ? "Equilibrium" : false,
 			c = :red
--- utils/dynamicalsystems.jl
+++ utils/dynamicalsystems.jl
@@ -1,45 +1,68 @@
+function isoutdomain(z::Vector{<:Real}, domain::Vector{Tuple{Float64, Float64}})
+	for (i, zᵢ) ∈ enumerate(z)
+		l, u = domain[i]
+		if zᵢ < l || zᵢ > u
+			return true
+		end
+	end
+
+	return false
+end
+
+# FIXME: How to deal with dimensions larger than 2
 computemanifolds(F!, DF!, steadystates; kwargs...) = computemanifolds(F!, DF!, steadystates, Float64[]; kwargs...)
 	function computemanifolds(
 	    F!::Function, DF!::Function,
 	    steadystates::Vector{Vector{Float64}},
 	    p::Vector{Any};
-	    T = 100, limit = 1000, dt = 0.01, h = 1e-3,
+		alg = TanYam7(),
+		abstol = 1.0e-8,
+		reltol = 1.0e-8,
+		h = 1e-3, dt = 1e-2,
+		tend = 6.,
+		domain = repeat([(-Inf, Inf)], length(first(steadystates))),
 	    kwargs...)
+
+		solver = (alg = alg, 
+			abstol = abstol, reltol = reltol, 
+			dt = dt, 
+			isoutofdomain = (z, p, t) -> isoutdomain(z, domain)
+		)
 	
-	    m = length(steadystates)
+	    s = length(steadystates)
 	    n = length(first(steadystates))
+
+		odefn = ODEFunction(F!; jac = DF!)
+
+		T = ceil(Int64, tend / dt)
 	
-	    function Finv!(dx, x, p, t)
-	        F!(dx, x, p, t)
-	        dx .= -dx
-	    end
-	
-	    fwds = CoupledODEs(F!, zeros(n), p)
-	    bckds = CoupledODEs(Finv!, zeros(n), p)
-	
-	    manifolds = NaN * ones(m, 2, 2, T, n)
+	    manifolds = NaN * ones(s, 2, 2, T, n)
 	        
 	    for (j, x̄) ∈ enumerate(steadystates)
 	        J = zeros(n, n); DF!(J, x̄, p, 0.0)
+
 	        λ, V = eigen(J)
+
+			# manifold = Vector{Float64}(undef, T, )
 	
 	        for (i, vᵢ) ∈ enumerate(eachcol(V))
+				if any(imag(vᵢ) .!= 0)
+					# TODO: How to handle imaginary eigenvalues?
+					continue
+				end
+
 	            isstable = real(λ[i]) < 0 # Stable if real part of eigenvalue is negative
-	
-	            ds = isstable ? bckds : fwds
-	            
-	            for (k, op) ∈ enumerate([-, +])
-	                reinit!(ds, op(x̄, vᵢ * h))
-	
-	                for t ∈ 1:T
-	                    step!(ds, dt)
-	                    xₙ = ds.integ.u
-	
-	                    manifolds[j, i, k, t, :] = xₙ
-	
-	                    if norm(xₙ) > limit break end
-	                end         
-	            end
+
+				tspace = isstable ? (0., -tend) : (0. : tend)
+
+				u₀⁻ = x̄ - h * vᵢ
+				u₀⁺ = x̄ + h * vᵢ
+
+				prob⁻ = ODEProblem(odefn, u₀⁻, tspace, p)
+				prob⁺ = ODEProblem(odefn, u₀⁺, tspace, p)
+
+				sol⁻ = solve(prob⁻; solver...)
+				sol⁺ = solve(prob⁺; solver...)
 	        end
 	    end
 	
