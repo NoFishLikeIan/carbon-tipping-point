6354aef4eb4f9e9b85f818f130966e2262ad915d
NoFishLikeIan <andreatitton96@gmail.com>
2023-07-28 10:14:09 +0200

    🦉 Lux attempt

--- scripts/valuefunctions/economystable.jl
+++ scripts/valuefunctions/economystable.jl
@@ -3,8 +3,10 @@ using DotEnv, JLD2
 
 using Lux
 using NeuralPDE
-using Optimization, OptimizationOptimJL
+using Optimization, OptimizationOptimJL, OptimizationOptimisers
 using ModelingToolkit: Interval, infimum, supremum
+
+using Roots
 
 include("../../src/model/climate.jl")
 include("../../src/model/economy.jl")
@@ -17,62 +19,59 @@ DATAPATH = get(env, "DATAPATH", "data")
 
 # Solve simplified model
 economy = Economy()
-@unpack A₀, δₖᵖ, κ, ϱ, θ = economy
+@unpack A, δₖᵖ, κ, θ = economy
 
-@parameters t, y 
+@parameters y 
 @variables v(..), χ(..)
 
 Dy = Differential(y)
-Dt = Differential(t)   
 
-ϕ(t, χ) = ϕ(χ, A(t, economy), economy)
-ϕ′(t, χ) = ϕ′(χ, A(t, economy), economy)
+ϕ(χ) = ϕ(χ, economy)
+ϕ′(χ) = ϕ′(χ, economy)
 f(χ, y, u) = f(χ * exp(y), u, economy)
 f′(χ, y, u) = ∂f_∂c(χ * exp(y), u, economy)
 
+# Steady state
+χ̄ = find_zero(χ -> ϕ(χ, economy) - δₖᵖ, (0, 1))
+
 eqs = [
-    f(χ(t, y), y, v(t, y)) + Dy(v(t, y)) * (ϕ(t, χ(t, y)) - δₖᵖ) + Dt(v(t, y))~ 0, # HBJ
-    f′(χ(t, y), y, v(t, y)) + Dy(v(t, y)) * ϕ′(t, χ(t, y)) ~ 0 # FOC
+    f(χ(y), y, v(y)) + Dy(v(y)) * (ϕ(χ(y)) - δₖᵖ) ~ 0, # HJB
+    f′(χ(y), y, v(y)) + Dy(v(y)) * ϕ′(χ(y)) ~ 0 # FOC
 ]
 
-T = 380.
+εbump = 1e-2
+bcs = [
+    bump(χ(y) - χ̄, εbump) * Dy(v(y)) ~ 0
+]
+
 y₀ = log(economy.Y₀)
 ymin, ymax = (0.1, 10) .* y₀
+domains = [ y ∈ Interval(ymin, ymax) ]
 
-bcs = [
-    Dy(v(T, y)) ~ 0,
-    Dt(v(T, y)) ~ -f(χ(T, y), y, v(T, y))
-]
-
-domains = [ 
-    t ∈ Interval(0., T),
-    y ∈ Interval(ymin, ymax) 
-]
-
-hiddensize = 80
+hiddensize = 18
 inputsize = length(domains)
 chain = [
     Lux.Chain(
-        Lux.Dense(inputsize, hiddensize, Lux.relu),
-        Lux.Dense(hiddensize, hiddensize, Lux.relu),
-        Lux.Dense(hiddensize, hiddensize, Lux.relu),
+        Lux.Dense(inputsize, hiddensize, Lux.tanh),
+        Lux.Dense(hiddensize, hiddensize, Lux.tanh),
+        Lux.Dense(hiddensize, hiddensize, Lux.tanh),
         Lux.Dense(hiddensize, 1)
-    ), # Neural network for v(t, y)
+    ), # Neural network for v(y)
     Lux.Chain(
-        Lux.Dense(inputsize, hiddensize, Lux.relu),
-        Lux.Dense(hiddensize, hiddensize, Lux.relu),
-        Lux.Dense(hiddensize, hiddensize, Lux.relu),
+        Lux.Dense(inputsize, hiddensize, Lux.σ),
+        Lux.Dense(hiddensize, hiddensize, Lux.σ),
+        Lux.Dense(hiddensize, hiddensize, Lux.σ),
         Lux.Dense(hiddensize, 1, Lux.σ)
-    ), # Neural network for χ(t, y)
+    ), # Neural network for χ(y)
 ]
 
 discretization = PhysicsInformedNN(chain, QuadratureTraining())
 
-@named pdesystem = PDESystem(eqs, bcs, domains, [t, y], [v(t, y), χ(t, y)])
+@named pdesystem = PDESystem(eqs, bcs, domains, [y], [v(y), χ(y)])
 prob = discretize(pdesystem, discretization)
 
 begin # logging
-    const losses = Float64[]
+    losses = Float64[]
     sym_prob = NeuralPDE.symbolic_discretize(pdesystem, discretization)
 
     pdeloss = sym_prob.loss_functions.pde_loss_functions
@@ -82,26 +81,27 @@ begin # logging
         push!(losses, l)
         print("Total loss: ", l, '\r')
         
-        if length(losses) % 10 == 0
-            println("HJB loss: ", map(l_ -> l_(p), pdeloss))
+        if length(losses) % 50 == 0
+            println("\nHJB loss: ", map(l_ -> l_(p), pdeloss))
             println("Boundary loss: ", map(l_ -> l_(p), bcsloss))
-            println("-------")
         end
 
         return false
     end
 end
 
-opt = OptimizationOptimJL.BFGS()
-result = Optimization.solve(prob, opt; callback = callback)
+result = Optimization.solve(
+    prob, BFGS(); callback = callback, maxiters = 2000
+); println("\nReturn code: ", result.retcode)
 
-println("Return code: ", result.retcode)
+begin # Save results
+    resultpath = joinpath(DATAPATH, "nn", "economynn.jld2")
 
-resultpath = joinpath(DATAPATH, "nn", "economynn.jld2")
+    println("Saving into ", resultpath)
+    save(resultpath, Dict(
+        "phi" => discretization.phi,
+        "depvar" => result.u.depvar,
+        "domains" => domains
+    ))
+end
 
-println("Saving into ", resultpath)
-save(resultpath, Dict(
-    "phi" => discretization.phi,
-    "depvar" => result.u.depvar,
-    "domains" => domains
-))
