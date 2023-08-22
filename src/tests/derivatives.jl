27fda63f0277434cce461d1068fa1436147e8d7b
NoFishLikeIan <andreatitton96@gmail.com>
2023-08-22 19:08:18 +0200

    🪐 5d gradient implementation

--- src/tests/derivatives.jl
+++ src/tests/derivatives.jl
@@ -1,6 +1,5 @@
-using NNlib
-using SparseArrays
 using Test: @test
+using BenchmarkTools
 
 include("../utils/derivatives.jl")
 
@@ -17,19 +16,65 @@ f(x) = exp(sin(x))
 f′(x) = cos(x) * exp(sin(x))
 f′′(x) = exp(sin(x)) * (cos(x)^2 - sin(x)) 
 
-@time ∂(f.(x))
-@time ∂²(f.(x))
+@time ∂(f.(x));
+@time ∂²(f.(x));
 
 @test all(abs.(∂(f.(x)) - f′.(x)[:, idxtwopad]) .< ε¹)
 @test all(abs.(∂²(f.(x)) - f′′.(x)[:, idxonepad]) .< ε²)
 
 # Two dimensions
-w₂ = reshape([0.2f0, 3f0], 1, 2)
+w₂ = reshape([2f-1, 3f0], 1, 2)
 
 g(x, y) = exp(sin(x * y))
 ∇g(x, y) = exp(sin(x * y)) * cos(x * y) * (w₂[1] * x + w₂[2] * y)
 
 ĝ = g.(x', x)
 
-@time ∇(ĝ, w₂)
-@test all(abs.(∇(ĝ, w₂) .- ∇g.(x', x)[idxtwopad, idxtwopad]) .< ε¹)
+@time S₂¹(w₂);
+@time ∇₂(ĝ, w₂);
+
+@test all(abs.(∇₂(ĝ, w₂) .- ∇g.(x', x)[idxtwopad, idxtwopad]) .< ε¹)
+
+# Five dimensions
+xsmall = paddedrange(0.5f0, 3ϵ + 0.5f0)
+idxpadsmall = 3:(length(xsmall) - 2)
+
+# Generating data 
+w₅ = reshape([1f0, 3f0, 2f0, 1f0, 1f0], 1, 5)
+
+h(a, b, c, d, e) = exp(sin(a * b * c * d * e))
+∇h(a, b, c, d, e) = cos(a * b * c * d * e) * exp(a * b * c * d * e) * [ b * c * d * e, a * c * d * e, a * b * d * e, a * b * c * e, a * b * c * d]
+
+X = Iterators.product(fill(vec(xsmall), 5)...) |> collect |> vec;
+
+y = reshape((z -> h.(z...)).(X), 1, length(X));
+y′ = reshape((z -> first(w₅ * ∇h.(z...))).(Iterators.product(fill(vec(xsmall), 5)...)), 1, length(X));
+
+# Testing indexing
+d = size(w₅, 2)
+n = size(y, 2)
+
+Δ = floor(Int, n^(1 / d)) # Assumes n = |x|^5 
+
+for a in eachcol(rand(1:5, 5, 5))
+    @test all(X[baseidx(Δ, Tuple(a))] .- X[1] .≈ Tuple(ϵ * (a .- 1)))
+end
+
+for jdx in Iterators.product(fill(vec(1:Δ), 5)...)
+    @test all(invbaseidx(Δ, baseidx(Δ, jdx)) .≈ jdx)
+end
+
+@time D = ∇₅(y, w₅);
+
+Darray = reshape(D, Δ, Δ, Δ, Δ, Δ);
+y′array = reshape(y′, Δ, Δ, Δ, Δ, Δ);
+
+@test all(abs.((Darray - y′array)[idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall]) .< ε¹)
+
+# Size testing
+xlarge = paddedrange(0f0, 0.1f0)
+d = length(xlarge)
+M = reshape(rand(Float32, d, d, d, d, d), 1, d^5);
+
+
+@btime ∇₅!(M, M, w₅);
--- src/utils/derivatives.jl
+++ src/utils/derivatives.jl
@@ -1,24 +1,146 @@
+using NNlib: conv
+using LoopVectorization: @tturbo, @turbo
+
 const ϵ = cbrt(eps(Float32))
 const ϵ⁻¹ = inv(ϵ)
 const ϵ⁻² = inv(ϵ^2)
 
-function paddedrange(from::Float32, to::Float32)
-    collect(range(from - 2ϵ, to + 2ϵ; step = ϵ)[:, :]')
+function paddedrange(from::Float32, to::Float32; pad = 2ϵ)
+    collect(range(from - pad, to + pad; step = ϵ)[:, :]')
 end
 
-const S₁¹ = reshape(ϵ⁻¹ .* [-1f0, 8f0, 0f0, -8f0, 1f0] ./ 12f0, 1, 5, 1, 1)
-const S₁² = reshape(ϵ⁻² .* [-1f0, 16f0, -30f0, 16f0, -1f0] ./ 12f0, 1, 5, 1, 1)
+const s¹ = [-1f0, 8f0, 0f0, -8f0, 1f0] ./ 12f0 # stencil of first derivative
+const d¹ = length(s¹)
+const s² = [1f0, -2f0, 1f0] # stencil of second derivative
+
+# One dimension
+const S₁¹ = reshape(ϵ⁻¹ .* s¹, 1, length(s¹), 1, 1)
+const S₁² = reshape(ϵ⁻² .* s², 1, length(s²), 1, 1)
 
 function ∂(y)
-    reshape(
-        NNlib.conv(reshape(y, 1, size(y, 2), 1, 1), S₁¹),
-        1, (size(y, 2) - 4)
-    )
+    dropdims(conv(reshape(y, 1, size(y, 2), 1, 1), S₁¹), dims = (3, 4))
 end
 
 function ∂²(y)
-    reshape(
-        NNlib.conv(reshape(y, 1, size(y, 2), 1, 1), S₁²),
-        1, (size(y, 2) - 4)
+    dropdims(conv(reshape(y, 1, size(y, 2), 1, 1), S₁²), dims = (3, 4))
+end
+
+# Two dimensions
+"2d, gradient sixteen-point stencil in direction w, with dims(w) = (1, 2)"
+function S₂¹(w::Matrix{Float32}) 
+    S = zeros(Float32, d¹, d¹, 1, 1)
+    S[3, :, :, :] .+= (ϵ⁻¹ * w[1]) .* s¹
+    S[:, 3, :, :] .+= (ϵ⁻¹ * w[2]) .* s¹
+
+    return S
+end
+
+"Gradient ∇ₓy in direction dims(w) = (1, 2)"
+function ∇₂(y::Matrix{Float32}, w::Matrix{Float32})
+    dropdims(
+        conv(reshape(y, size(y, 1), size(y, 2), 1, 1), S₂¹(w)),
+        dims = (3, 4)
     )
+end
+
+
+# Five dimensions
+"5d, gradient sixteen-point stencil in direction w, with dims(w) = (1, 5)"
+function S₅¹(w::Matrix{Float32}) 
+    # Ugliest function I have ever written
+    S = zeros(Float32, d¹, d¹, d¹, d¹, d¹, 1, 1)
+    S[:, :, :, :, 1 + d¹ ÷ 2, :, :] .+= (ϵ⁻¹ * w[1]) .* s¹
+    S[:, :, :, 1 + d¹ ÷ 2, :, :, :] .+= (ϵ⁻¹ * w[2]) .* s¹
+    S[:, :, 1 + d¹ ÷ 2, :, :, :, :] .+= (ϵ⁻¹ * w[3]) .* s¹
+    S[:, 1 + d¹ ÷ 2, :, :, :, :, :] .+= (ϵ⁻¹ * w[4]) .* s¹
+    S[1 + d¹ ÷ 2, :, :, :, :, :, :] .+= (ϵ⁻¹ * w[5]) .* s¹
+
+    return S
+end
+
+baseidx(Δ::Int, a::NTuple{5, Int})::Int = 1 + (a[1] - 1) + (a[2] - 1) * Δ + (a[3] - 1) * Δ^2 + (a[4] - 1) * Δ^3 + (a[5] - 1) * Δ^4
+
+function invbaseidx(Δ::Int, jdx::Int)::NTuple{5, Int}
+    i = 1 + mod(jdx - 1, Δ)
+    j = 1 + mod((jdx - i) ÷ Δ, Δ)
+    k = 1 + mod((jdx - i - (j - 1) * Δ) ÷ (Δ^2), Δ)
+    l = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ^2) ÷ (Δ^3), Δ)
+    m = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ^2 - (l - 1) * Δ^3) ÷ (Δ^4), Δ)
+
+    return (i, j, k, l, m)
+end
+
+"
+Five stencil ∇ₓy in direction dims(w) = (1, 5). Assume y is a (1, n) matrix with entries y = { f(x[i, j, k, l, m]) }ₙ.
+"
+∇₅(y::Matrix{Float32}, w::Matrix{Float32}) = ∇₅!(similar(y), y, w)
+function ∇₅!(D::Matrix{Float32}, y::Matrix{Float32}, w::Matrix{Float32})
+
+    d = size(w, 2)
+    n = size(y, 2)
+
+    Δ = floor(Int, n^(1 / d)) # Assumes n = Δ^5; |x| ≡ Δ 
+
+    Δ², Δ³, Δ⁴ = Δ^2, Δ^3, Δ^4
+    
+    @tturbo for jdx in axes(D, 2)   
+        i = 1 + mod(jdx - 1, Δ)
+        j = 1 + mod((jdx - i) ÷ Δ, Δ)
+        k = 1 + mod((jdx - i - (j - 1) * Δ) ÷ (Δ²), Δ)
+        l = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ²) ÷ (Δ³), Δ)
+        m = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ² - (l - 1) * Δ³) ÷ Δ⁴, Δ)
+
+        jklm = jdx - (i - 1)
+        iklm = jdx - (j - 1) * Δ
+        ijlm = jdx - (k - 1) * Δ²
+        ijkm = jdx - (l - 1) * Δ³
+        ijkl = jdx - (m - 1) * Δ⁴
+
+    
+        D[jdx] = (ϵ⁻¹ / 12f0) * (  
+            w[1] * (   
+                8f0 * (
+                    y[jklm + min(i + 1, Δ) - 1] -
+                    y[jklm + max(i - 1, 1) - 1]
+                ) - (
+                    y[jklm + min(i + 2, Δ) - 1] - 
+                    y[jklm + max(i - 2, 1) - 1]
+                )
+            ) + w[2] * (   
+                8f0 * (
+                    y[iklm + Δ * (min(j + 1, Δ) - 1)] -
+                    y[iklm + Δ * (max(j - 1, 1) - 1)]
+                ) - (
+                    y[iklm + Δ * (min(j + 2, Δ) - 1)] - 
+                    y[iklm + Δ * (max(j - 2, 1) - 1)]
+                )
+            ) + w[3] * (   
+                8f0 * (
+                    y[ijlm + Δ² * (min(k + 1, Δ) - 1)] -
+                    y[ijlm + Δ² * (max(k - 1, 1) - 1)]
+                ) - (
+                    y[ijlm + Δ² * (min(k + 2, Δ) - 1)] - 
+                    y[ijlm + Δ² * (max(k - 2, 1) - 1)]
+                )
+            ) + w[4] * (   
+                8f0 * (
+                    y[ijkm + Δ³ * (min(l + 1, Δ) - 1)] -
+                    y[ijkm + Δ³ * (max(l - 1, 1) - 1)]
+                ) - (
+                    y[ijkm + Δ³ * (min(l + 2, Δ) - 1)] - 
+                    y[ijkm + Δ³ * (max(l - 2, 1) - 1)]
+                )
+            )  + w[5] *  (   
+                8f0 * (
+                    y[ijkl + Δ⁴ * (min(m + 1, Δ) - 1)] -
+                    y[ijkl + Δ⁴ * (max(m - 1, 1) - 1)]
+                ) - (
+                    y[ijkl + Δ⁴ * (min(m + 2, Δ) - 1)] - 
+                    y[ijkl + Δ⁴ * (max(m - 2, 1) - 1)]
+                )
+            )
+        )
+    end
+
+    return D 
 end
