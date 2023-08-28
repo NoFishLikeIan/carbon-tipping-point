843019b833d487bdf2cc4c347be4ca5da98788c3
NoFishLikeIan <andreatitton96@gmail.com>
2023-08-28 11:32:35 +0200

    🦉 second derivative and directional gradient

--- src/tests/derivatives.jl
+++ src/tests/derivatives.jl
@@ -36,8 +36,9 @@ ĝ = g.(x', x)
 @test all(abs.(∇₂(ĝ, w₂) .- ∇g.(x', x)[idxtwopad, idxtwopad]) .< ε¹)
 
 # Five dimensions
-xsmall = paddedrange(0.5f0, 3ϵ + 0.5f0)
+xsmall = paddedrange(0.5f0, 0.55f0)
 idxpadsmall = 3:(length(xsmall) - 2)
+Δ = length(xsmall)
 
 # Generating data 
 w₅ = reshape([1f0, 3f0, 2f0, 1f0, 1f0], 1, 5)
@@ -51,19 +52,6 @@ y = reshape((z -> h.(z...)).(X), 1, length(X));
 y′ = reshape((z -> first(w₅ * ∇h.(z...))).(Iterators.product(fill(vec(xsmall), 5)...)), 1, length(X));
 
 # Testing indexing
-d = size(w₅, 2)
-n = size(y, 2)
-
-Δ = floor(Int, n^(1 / d)) # Assumes n = |x|^5 
-
-for a in eachcol(rand(1:5, 5, 5))
-    @test all(X[baseidx(Δ, Tuple(a))] .- X[1] .≈ Tuple(ϵ * (a .- 1)))
-end
-
-for jdx in Iterators.product(fill(vec(1:Δ), 5)...)
-    @test all(invbaseidx(Δ, baseidx(Δ, jdx)) .≈ jdx)
-end
-
 @time D = ∇₅(y, w₅);
 
 Darray = reshape(D, Δ, Δ, Δ, Δ, Δ);
@@ -76,5 +64,55 @@ xlarge = paddedrange(0f0, 0.1f0)
 d = length(xlarge)
 M = reshape(rand(Float32, d, d, d, d, d), 1, d^5);
 
-
 @btime ∇₅!(M, M, w₅);
+
+# Second derivative
+∂²₁h(a, b, c, d, e) = -h(a, b, c, d, e) * (b * c * d * e)^2 * (sin(a * b * c * d * e) - cos(a * b * c * d * e)^2)
+
+y′′ = reshape((z -> ∂²₁h.(z...)).(X), 1, length(X));
+
+@time D = ∂²₁(y);
+
+y′′array = reshape(y′′, Δ, Δ, Δ, Δ, Δ);
+Darray = reshape(D, Δ, Δ, Δ, Δ, Δ);
+
+errors = abs.((Darray - y′′array)[idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall]);
+
+@test all(errors .< ε²)
+
+# Directional derivative
+w = ones(Float32, 1, 5)
+Fα = ones(Float32, 1, 2)
+Fχ = ones(Float32, 1, 1)
+
+V = copy(y);
+
+@time DV = ∇V′w(V, w, Fα, Fχ);
+
+diry′ = similar(DV);
+
+for col in axes(diry′, 2)
+    ∇ᵢ = ∇h(X[col]...)
+    diry′[1, col] = (w * ∇ᵢ)[1]
+    diry′[2, col] = (Fα * ∇ᵢ[[3, 4]])[1]
+    diry′[3, col] = (Fχ * ∇ᵢ[[4]])[1]
+end
+
+for dim in 1:3
+    DVmatrix = reshape(DV[dim, :], Δ, Δ, Δ, Δ, Δ);
+    diry′matrix = reshape(diry′[dim, :], Δ, Δ, Δ, Δ, Δ);
+
+    errors = abs.((DVmatrix - diry′matrix)[idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall]);
+
+    @test all(errors .< ε¹)
+end
+
+# Size testing
+w = rand(Float32, 1, 5)
+Fα = rand(Float32, 1, 2)
+Fχ = rand(Float32, 1, 1)
+
+d = length(xlarge)
+M = reshape(rand(Float32, d, d, d, d, d), 1, d^5);
+
+@btime ∇V′w!(M, M, w, Fα, Fχ);
--- src/utils/derivatives.jl
+++ src/utils/derivatives.jl
@@ -45,41 +45,10 @@ end
 
 
 # Five dimensions
-"5d, gradient sixteen-point stencil in direction w, with dims(w) = (1, 5)"
-function S₅¹(w::Matrix{Float32}) 
-    # Ugliest function I have ever written
-    S = zeros(Float32, d¹, d¹, d¹, d¹, d¹, 1, 1)
-    S[:, :, :, :, 1 + d¹ ÷ 2, :, :] .+= (ϵ⁻¹ * w[1]) .* s¹
-    S[:, :, :, 1 + d¹ ÷ 2, :, :, :] .+= (ϵ⁻¹ * w[2]) .* s¹
-    S[:, :, 1 + d¹ ÷ 2, :, :, :, :] .+= (ϵ⁻¹ * w[3]) .* s¹
-    S[:, 1 + d¹ ÷ 2, :, :, :, :, :] .+= (ϵ⁻¹ * w[4]) .* s¹
-    S[1 + d¹ ÷ 2, :, :, :, :, :, :] .+= (ϵ⁻¹ * w[5]) .* s¹
-
-    return S
-end
-
-baseidx(Δ::Int, a::NTuple{5, Int})::Int = 1 + (a[1] - 1) + (a[2] - 1) * Δ + (a[3] - 1) * Δ^2 + (a[4] - 1) * Δ^3 + (a[5] - 1) * Δ^4
-
-function invbaseidx(Δ::Int, jdx::Int)::NTuple{5, Int}
-    i = 1 + mod(jdx - 1, Δ)
-    j = 1 + mod((jdx - i) ÷ Δ, Δ)
-    k = 1 + mod((jdx - i - (j - 1) * Δ) ÷ (Δ^2), Δ)
-    l = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ^2) ÷ (Δ^3), Δ)
-    m = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ^2 - (l - 1) * Δ^3) ÷ (Δ^4), Δ)
-
-    return (i, j, k, l, m)
-end
-
-"
-Five stencil ∇ₓy in direction dims(w) = (1, 5). Assume y is a (1, n) matrix with entries y = { f(x[i, j, k, l, m]) }ₙ.
-"
+"Five stencil ∇ₓy in direction dims(w) = (1, 5). Assume y is a (1, n) matrix with entries y = { f(x[i, j, k, l, m]) }ₙ."
 ∇₅(y::Matrix{Float32}, w::Matrix{Float32}) = ∇₅!(similar(y), y, w)
 function ∇₅!(D::Matrix{Float32}, y::Matrix{Float32}, w::Matrix{Float32})
-
-    d = size(w, 2)
-    n = size(y, 2)
-
-    Δ = floor(Int, n^(1 / d)) # Assumes n = Δ^5; |x| ≡ Δ 
+    Δ = floor(Int, size(y, 2)^(1 / 5)) # Assumes n = Δ^5; |x| ≡ Δ 
 
     Δ², Δ³, Δ⁴ = Δ^2, Δ^3, Δ^4
     
@@ -139,6 +108,115 @@ function ∇₅!(D::Matrix{Float32}, y::Matrix{Float32}, w::Matrix{Float32})
                     y[ijkl + Δ⁴ * (max(m - 2, 1) - 1)]
                 )
             )
+        )
+    end
+
+    return D 
+end
+
+# ---- Ad hoc functions ----
+
+"
+Takes a row major V (1 × n) and three directions w (1 × 5), Fα (1 × 2), and Fχ (1 × 1). 
+
+Computes the three necessary gradients:
+1. HJB: ∇V⋅w
+2. FOC α: ∇V₃₄⋅Fα
+3. FOC χ: ∇V₄⋅Fχ
+
+The output is a row major directional derivative matrix D (3 × n).
+"
+∇V′w(V::Matrix{Float32}, w::Matrix{Float32}, Fα::Matrix{Float32}, Fχ::Matrix{Float32}) = ∇V′w!(Matrix{Float32}(undef, 3, size(V, 2)), V, w, Fα, Fχ)
+function ∇V′w!(D::Matrix{Float32}, V::Matrix{Float32}, w::Matrix{Float32}, Fα::Matrix{Float32}, Fχ::Matrix{Float32})::Matrix{Float32}
+    Δ = floor(Int, size(V, 2)^(1 / 5)) # Assumes the grid for V is Δ^5; |x| ≡ Δ
+
+    Δ², Δ³, Δ⁴ = Δ^2, Δ^3, Δ^4
+    
+    # Iteration for w
+    @tturbo for jdx in axes(D, 2)   
+        i = 1 + mod(jdx - 1, Δ)
+        j = 1 + mod((jdx - i) ÷ Δ, Δ)
+        k = 1 + mod((jdx - i - (j - 1) * Δ) ÷ (Δ²), Δ)
+        l = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ²) ÷ (Δ³), Δ)
+        m = 1 + mod((jdx - i - (j - 1) * Δ - (k - 1) * Δ² - (l - 1) * Δ³) ÷ Δ⁴, Δ)
+
+        jklm = jdx - (i - 1)
+        iklm = jdx - (j - 1) * Δ
+        ijlm = jdx - (k - 1) * Δ²
+        ijkm = jdx - (l - 1) * Δ³
+        ijkl = jdx - (m - 1) * Δ⁴
+
+        ∂₃ = 8f0 * (
+            V[ijlm + Δ² * (min(k + 1, Δ) - 1)] -
+            V[ijlm + Δ² * (max(k - 1, 1) - 1)]
+        ) - (
+            V[ijlm + Δ² * (min(k + 2, Δ) - 1)] - 
+            V[ijlm + Δ² * (max(k - 2, 1) - 1)]
+        )
+
+        ∂₄ = 8f0 * (
+            V[ijkm + Δ³ * (min(l + 1, Δ) - 1)] -
+            V[ijkm + Δ³ * (max(l - 1, 1) - 1)]
+        ) - (
+            V[ijkm + Δ³ * (min(l + 2, Δ) - 1)] - 
+            V[ijkm + Δ³ * (max(l - 2, 1) - 1)]
+        )
+
+        # ∇V⋅w
+        D[1, jdx] = (ϵ⁻¹ / 12f0) * (
+            # Dimension T  
+            w[1] * (   
+                8f0 * (
+                    V[jklm + min(i + 1, Δ) - 1] -
+                    V[jklm + max(i - 1, 1) - 1]
+                ) - (
+                    V[jklm + min(i + 2, Δ) - 1] - 
+                    V[jklm + max(i - 2, 1) - 1]
+                )
+            ) + 
+            # Dimension N
+            w[2] * (   
+                8f0 * (
+                    V[iklm + Δ * (min(j + 1, Δ) - 1)] -
+                    V[iklm + Δ * (max(j - 1, 1) - 1)]
+                ) - (
+                    V[iklm + Δ * (min(j + 2, Δ) - 1)] - 
+                    V[iklm + Δ * (max(j - 2, 1) - 1)]
+                )
+            ) + 
+            # Dimension m
+            w[3] * ∂₃ + 
+            # Dimension y
+            w[4] * ∂₄ +  
+            # Dimension t, use forward difference
+            w[5] *  (   
+                24f0V[ijkl + Δ⁴ * (min(m + 1, Δ) - 1)] - 
+                6f0V[ijkl + Δ⁴ * (min(m + 2, Δ) - 1)] -
+                18f0V[jdx]
+            )
+        )
+
+        D[2, jdx] = (ϵ⁻¹ / 12f0) * (∂₃ * Fα[1] + ∂₄ * Fα[2]) # ∇V₃₄⋅Fα
+        D[3, jdx] = (ϵ⁻¹ / 12f0) * ∂₄ * Fχ[1] # ∇V₄ ⋅ Fχ
+    end
+
+    return D
+end
+
+"Compute second derivative of y in direction of the first input x₁:
+( ∂²f(x₁, x₂, x₃, x₄, x₅) / ∂x₁² )"
+∂²₁(y::Matrix{Float32}) = ∂²₁!(similar(y), y)
+function ∂²₁!(D::Matrix{Float32}, y::Matrix{Float32})
+    Δ = floor(Int, size(y, 2)^(1 / 5)) # Assumes n = Δ^5; |x| ≡ Δ 
+    
+    D .= (-2f0 * ϵ⁻²) .* y
+    
+    @tturbo for jdx in axes(D, 2)   
+        i = 1 + mod(jdx - 1, Δ)
+        jklm = jdx - (i - 1)
+
+        D[jdx] += ϵ⁻² * (
+            y[jklm + min(i + 1, Δ) - 1] + y[jklm + max(i - 1, 1) - 1]
         )
     end
 
