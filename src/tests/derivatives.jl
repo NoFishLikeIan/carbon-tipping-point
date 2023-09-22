7bc77d3cfaedb65dc216da32e3223901195cb32f
NoFishLikeIan <andreatitton96@gmail.com>
2023-09-22 14:14:36 +0200

    🪐 implemented central differences

--- src/tests/derivatives.jl
+++ src/tests/derivatives.jl
@@ -3,58 +3,77 @@ using BenchmarkTools
 
 include("../utils/derivatives.jl")
 
-# Test
+# Tolerance for first and second derivatives
 ε¹ = 1f-3
 ε² = 4f-2
 
-# Four dimensions
-xsmall = paddedrange(0.5f0, 0.7f0)
-idxpadsmall = 3:(length(xsmall) - 2)
-Δ = length(xsmall)
+begin # Initialise three dimensional cube
+    dims = 3 # state space excluding time
 
-# Generating data 
-h(a, b, c, d) = exp(sin(a * b * c * d))
-∇h(a, b, c, d) = cos(a * b * c * d) * exp(a * b * c * d) * [ b * c * d, a * c * d, a * b * d, a * b * c]
+    span = paddedrange(0f0, 1f0)
+    idxpadsmall = 3:(length(span) - 2)
+    Δ = length(span)
 
-X = Iterators.product(fill(vec(xsmall), 4)...) |> collect |> vec;
+    X = Iterators.product(fill(span, dims)...) |> collect |> vec
+    n = length(X);
 
-V = reshape((z -> h.(z...)).(X), 1, length(X));
+    Xmat = Matrix{Float32}(undef, dims, n)
+    for (i, xᵢ) ∈ enumerate(X)
+        Xmat[:, i] .= xᵢ
+    end
+end;
 
-# Directional derivative
-w = ones(Float32, 3, size(V, 2));
+# Generating mock data
+v(T, m, y) = sin(T * y * m)
+∇v(T, m, y) = cos(T * m * y) .* [y * m, T * y, T * m]
 
-DV = similar(V, 4, size(V, 2));
-@time dir∇V!(DV, V, w);
+# Exact derivative
+V = Matrix{Float32}(undef, 1, n);
+V′ = Matrix{Float32}(undef, dims, n);
+for i ∈ axes(V, 2)
+    xᵢ = @view Xmat[:, i]
+    V[i] = v(xᵢ...)
+    V′[:, i] .= ∇v(xᵢ...)
+end;
 
-diry′ = similar(DV);
-for col in axes(diry′, 2)
-    ∇ᵢ = ∇h(X[col]...)
-    diry′[1, col] = ∇ᵢ[1]
-    diry′[2, col] = ∇ᵢ[3]
-    diry′[3, col] = ∇ᵢ[4]
-    diry′[4, col] = sum(∇ᵢ[2:4])
+# Central difference
+Dcentral = similar(V′);
+@time central∇V!(Dcentral, V);
+
+for dim in 1:dims
+    DVmatrix = reshape(Dcentral[dim, :], Δ, Δ, Δ);
+    matrix = reshape(V′[dim, :], Δ, Δ, Δ);
+
+    errors = abs.((DVmatrix - matrix)[idxpadsmall, idxpadsmall, idxpadsmall])
+    @test all(errors .< ε¹)
+    println("Dimension $dim: max ε = $(maximum(errors))")
 end
 
-for dim in 1:3
-    DVmatrix = reshape(DV[dim, :], Δ, Δ, Δ, Δ);
-    diry′matrix = reshape(diry′[dim, :], Δ, Δ, Δ, Δ);
+# Forward difference
+w = -1f0ones(Float32, dims, n);
+D = similar(V′);
+@time dir∇V!(D, V, w);
 
-    errors = abs.((DVmatrix - diry′matrix)[idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall]);
+for dim in 1:dims
+    Dmatrix = reshape(D[dim, :], Δ, Δ, Δ);
+    y′matrix = reshape(V′[dim, :], Δ, Δ, Δ);
+
+    errors = abs.((Dmatrix - y′matrix)[idxpadsmall, idxpadsmall, idxpadsmall]);
 
     @test all(errors .< ε¹)
+    println("Dimension $dim: max ε = $(maximum(errors))")
 end
 
-# Second derivative w.r.t. the second argument
-∂²₂h(a, b, c, d) = (a * c * d)^2 * (cos(a * b * c * d)^2 - sin(a * b * c * d)^2) * h(a, b, c, d)
+# Second derivative w.r.t. the first argument
+∂²₁h(T, m, y) = -(m * y)^2 * sin(T * m * y)
+y′′ = similar(D²);
+for col in axes(y′′, 2)
+    y′′[col] = ∂²₁h(X[col]...)
+end
 
 D² = similar(V);
 ∂²T!(D², V); @time ∂²T!(D², V);
 
-y′′ = similar(D²);
-for col in axes(y′′, 2)
-    y′′[col] = ∂²₂h(X[col]...)
-end
-
-errormatrix = reshape(abs.(y′′ - D²), Δ, Δ, Δ, Δ)[idxpadsmall, idxpadsmall, idxpadsmall, idxpadsmall];
+errormatrix = reshape(abs.(y′′ - D²), Δ, Δ, Δ)[idxpadsmall, idxpadsmall, idxpadsmall];
 
 @test all(errormatrix .< ε²)
--- src/utils/derivatives.jl
+++ src/utils/derivatives.jl
@@ -6,80 +6,103 @@ function paddedrange(from::Float32, to::Float32; pad = 2ϵ)
     collect(range(from - pad, to + pad; step = ϵ)[:, :]')
 end
 
+function ∂₊(V, idx, onestep, twostep)
+    (ϵ⁻¹ / 2f0) .* (-V[twostep] + 4f0V[onestep] - 3f0V[idx])
+end
+
+function ∂₋(V, idx, onestep, twostep)
+    (ϵ⁻¹ / 2f0) .* (3f0V[idx] - 4f0V[onestep] + V[twostep])
+end
+
 """
-Given a V (1 × n)  and a drift w (3 × n) returns a matrix D (4 × n) with rows:
-- D₁ = ∂tV always computed forward
-- D₂ = ∂mV
-- D₃ = ∂yV
-- D₄ = ∇V⋅w
+Given a Vₜ (1 × n)  and a drift w (3 × n) returns a matrix ∇Vₜ⋅w (1 × n). Note that Vₜ_ijk = Vₜ(T, m, y).
 
-The finite difference scheme is computed by using second order forward derivatives if the drift is positive and backwards if it is negative
+The finite difference scheme is computed by using second order forward derivatives if the drift is positive and backwards if it is negative.
 
-Note that V_tijk ≈ V(t, T, m, y)
+Accepts δ (3 × 1) which is the domain size. It defaults to δ = 1.
 """
 function dir∇V(V::Matrix{Float32}, w::Matrix{Float32})    
-    D = Matrix{Float32}(undef, 4, size(V, 2))
+    D = Matrix{Float32}(undef, size(w))
     return dir∇V!(D, V, w)
 end
 function dir∇V!(D::Matrix{Float32}, V::Matrix{Float32}, w::Matrix{Float32})
-    Δ = floor(Int, size(V, 2)^(1 / 4)) # Assumes n = Δ^4; |x| ≡ Δ 
-
-    Δ², Δ³ = Δ^2, Δ^3
-
+    m, n = size(w)
+    Δ = round(Int, n^(1 / m)) # Assumes n = Δ^m where |x| ≡ Δ 
+    Δ² = Δ^2
+ 
     posdrift = w .> 0
     
-    for index in axes(D, 2)  
-        t = 1 + mod(index - 1, Δ)
-        i = 1 + mod((index - t) ÷ Δ, Δ)
-        j = 1 + mod((index - t - (i - 1) * Δ) ÷ (Δ²), Δ)
-        k = 1 + mod((index - t - (i - 1) * Δ - (j - 1) * Δ²) ÷ (Δ³), Δ)
+    @inbounds for idx in axes(D, 2)  
+        i = 1 + mod(idx - 1, Δ)
+        j = 1 + mod((idx - i) ÷ Δ, Δ)
+        k = 1 + mod((idx - i - (j - 1) * Δ) ÷ (Δ²), Δ)
 
-        jkl = index - (t - 1)
-        tjk = index - (i - 1) * Δ
-        tik = index - (j - 1) * Δ²
-        tij = index - (k - 1) * Δ³
+        jk = idx - (i - 1)
+        ik = idx - (j - 1) * Δ
+        ij = idx - (k - 1) * Δ²
 
-        D[1, index] = ϵ⁻¹ .* (V[jkl + min(t + 1, Δ) - 1] - V[index]) # ∂t
 
-        D[2, index] = (ϵ⁻¹ / 2f0) .* (posdrift[2, index] ? 
-            -V[tik + Δ² * (min(j + 2, Δ) - 1)] + 4f0V[tik + Δ² * (min(j + 1, Δ) - 1)] - 3f0V[index] :
-            3f0V[index] - 4f0V[tik + Δ² * (max(j - 1, 1) - 1)] - V[tik + Δ² * (max(j - 2, 1) - 1)]
-        ) # ∂m
-     
-        D[3, index] = (ϵ⁻¹ / 2f0) .* (posdrift[3, index] ? 
-            -V[tij + Δ³ * (min(k + 2, Δ) - 1)] + 4f0V[tij + Δ³ * (min(k + 1, Δ) - 1)] - 3f0V[index] :
-            3f0V[index] - 4f0V[tij + Δ³ * (max(k - 1, 1) - 1)] - V[tij + Δ³ * (max(k - 2, 1) - 1)]
-        ) # ∂y
-    
-        D[4, index] = 
-            w[2, index] * D[2, index] + 
-            w[3, index] * D[3, index] + 
-            (ϵ⁻¹ / 2f0) .* (posdrift[2, index] ? 
-                -V[tjk + Δ * (min(i + 2, Δ) - 1)] + 4f0V[tjk + Δ * (min(i + 1, Δ) - 1)] - 3f0V[index] :
-                3f0V[index] - 4f0V[tjk + Δ * (max(i - 1, 1) - 1)] - V[tjk + Δ * (max(i - 2, 1) - 1)]
-            ) # ∂T
+        D[1, idx] = posdrift[1, idx] ? # ∂T
+                ∂₊(V, idx, jk + (min(i + 1, Δ) - 1), jk + (min(i + 2, Δ) - 1)) :
+                ∂₋(V, idx, jk + (max(i - 1, 1) - 1), jk + (max(i - 2, 1) - 1))
+
+        D[2, idx] = posdrift[2, idx] ? # ∂m
+                ∂₊(V, idx, ik + Δ * (min(j + 1, Δ) - 1), ik + Δ * (min(j + 2, Δ) - 1)) :
+                ∂₋(V, idx, ik + Δ * (max(j - 1, 1) - 1), ik + Δ * (max(j - 2, 1) - 1))
+
+        D[3, idx] = posdrift[3, idx] ? # ∂y
+                ∂₊(V, idx, ij + Δ² * (min(k + 1, Δ) - 1), ij + Δ² * (min(k + 2, Δ) - 1)) :
+                ∂₋(V, idx, ij + Δ² * (max(k - 1, 1) - 1), ij + Δ² * (max(k - 2, 1) - 1)) 
+    end
+
+    return D 
+end
+
+"""
+Given a V (1 × n) returns a matrix D (3 × n) with rows (∂TV, ∂mV, ∂yV) where and (∂TV, ∂mV, ∂yV) are computed via central differences. Note that V_ijk = V(T, m, y)
+"""
+function central∇V(V::Matrix{Float32})    
+    central∇V!(Matrix{Float32}(undef, 3, size(V, 2)), V)
+end
+function central∇V!(D::Matrix{Float32}, V::Matrix{Float32})
+    dims, n = size(D)
+    Δ = round(Int, n^(1 / dims)) # Assumes n = Δ^dims; |x| ≡ Δ 
+    Δ² = Δ^2
+
+    @inbounds for idx in axes(D, 2)  
+        i = 1 + mod(idx - 1, Δ)
+        j = 1 + mod((idx - i) ÷ Δ, Δ)
+        k = 1 + mod((idx - i - (j - 1) * Δ) ÷ (Δ²), Δ)
+
+        jk = idx - (i - 1)
+        ik = idx - (j - 1) * Δ
+        ij = idx - (k - 1) * Δ²
+
+        D[1, idx] = (ϵ⁻¹ / 2f0) .* (V[jk + (min(i + 1, Δ) - 1)] - V[jk + (max(i - 1, 1) - 1)])
+
+        D[2, idx] = (ϵ⁻¹ / 2f0) .* (V[ik + Δ * (min(j + 1, Δ) - 1)] - V[ik + Δ * (max(j - 1, 1) - 1)])
+
+        D[3, idx] = (ϵ⁻¹ / 2f0) .* (V[ij + Δ² * (min(k + 1, Δ) - 1)] - V[ij + Δ² * (max(k - 1, 1) - 1)])
     end
 
     return D 
 end
 
 "Compute second derivative of y in direction of the first input x₁:
-( ∂²f(x₁, x₂, x₃, x₄) / ∂x₂² )"
-function ∂²T(y)
-    D = similar(y)
-    ∂²T!(D, y)
-    return copy(D)
+( ∂²/(∂x₁)² f(x₁, x₂, x₃) )"
+function ∂²T(V::Matrix{Float32}; m = 3)
+    D = similar(V)
+    ∂²T!(D, V; m = m)
 end
-function ∂²T!(D, y)
-    Δ = floor(Int, size(y, 2)^(1 / 4)) # Assumes n = Δ^4; |x| ≡ Δ 
+function ∂²T!(D::Matrix{Float32}, V::Matrix{Float32}; m = 3)
+    Δ = round(Int, size(V, 2)^(1 / m)) # Assumes n = Δ^m; |x| ≡ Δ 
         
-    for index in axes(D, 2) 
-        t = 1 + mod(index - 1, Δ)
-        i = 1 + mod((index - t) ÷ Δ, Δ)
-        tjk = index - (i - 1) * Δ
+    @inbounds for idx in axes(D, 2) 
+        i = 1 + mod(idx - 1, Δ)
+        jk = idx - (i - 1)
 
-        D[index] = ϵ⁻² * (
-            y[tjk + Δ * (min(i + 1, Δ) - 1)] + y[tjk + Δ * (max(i - 1, 1) - 1)] - 2f0 * y[index]
+        D[idx] = ϵ⁻² * (
+            V[jk + (min(i + 1, Δ) - 1)] + V[jk + (max(i - 1, 1) - 1)] - 2f0 * V[idx]
         )
     end
 
