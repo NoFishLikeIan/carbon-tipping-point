35a515fd107d835d9b0084874881160394d00812
NoFishLikeIan <andreatitton96@gmail.com>
2023-10-06 17:24:01 +0200

    🪐 rkmethod for backward induction

--- scripts/valuefunction.jl
+++ scripts/valuefunction.jl
@@ -1,62 +1,67 @@
-using BenchmarkTools
-
 using UnPack  
 using StatsBase
 using NLsolve, Optim
 using LinearAlgebra
-using Base.Iterators: product
 
-include("../src/model/init.jl")
+include("../src/utils/grids.jl")
 include("../src/utils/derivatives.jl")
+include("../src/model/init.jl")
 
-# -- Test over a cube
-const dims = 3; # Dimension state space excluding time
-const Z₀ = reshape([hogg.T₀, log(hogg.M₀), log(economy.Y₀)], (dims, 1)) |> tounit;
-const grid = paddedrange(3ϵ, 1f0 - 3ϵ; pad = 2ϵ);
-const n = length(grid)^dims;
+# -- Generate state cube
+const statedomain::Vector{Domain} = [
+    (hogg.T₀, hogg.T̄, 60), 
+    (log(hogg.M₀), log(hogg.M̄), 60), 
+    (log(economy.Y̲), log(economy.Ȳ), 25)
+];
+const Ω = makeregulargrid(statedomain);
+const Nₛ = size(Ω);
+const X = fromgridtoarray(Ω);
 
-begin
-    const Z = Matrix{Float32}(undef, dims, n)
-    const U₀ = Matrix{Float32}(undef, 1, n)
+# -- Generate action square
+const actiondomain::Vector{Domain} = [
+    (1f-3, 1f0 - 1f-3, 11), (1f-3, 1f0 - 1f-3, 11)
+]
 
-    for (i, zᵢ) ∈ product(fill(grid, dims)...) |> enumerate
-        Z[:, i] .= zᵢ
-        U₀[i] = (zᵢ[3]^2 - zᵢ[1]^2 + zᵢ[2]^2) / 4f0 - 0.7f0
-    end
-end;
+const Γ = makeregulargrid(actiondomain);
+const Nₐ = size(Γ);
+const P = fromgridtoarray(Γ);
 
-const V₀ = U₀ .* economy.V̲
-const X = fromunit(Z);
-const ∇U₀ = central∇V(U₀); @time central∇V(U₀);
-const ∇V₀ = expand∂(∇U₀);
+G(t::Float32, V::FieldGrid, X::VectorGrid) = G!(copy(V), t, V, X);
+function G!(∂ₜV::FieldGrid, t::Float32, V::FieldGrid, X::VectorGrid)
+    ∇V = central∇(V, Ω);
+    policy = objectivefunctional(t, V, ∇V, X, P);
 
-const α₀ = abatement(economy.t₁, X, ∇V₀); @time abatement(economy.t₁, X, ∇V₀);
-const χ₀ = consumption(economy.t₁, V₀, ∇V₀); @time consumption(economy.t₁, V₀, ∇V₀);
-const w = drift(economy.t₁, X, α₀, χ₀); @time drift(economy.t₁, X, α₀, χ₀)
+    α = @views policy[:, :, :, 1]
+    χ = @views policy[:, :, :, 2]
 
-# TODO
-Vₜ = copy(V₀);
-
-∇(V::Matrix{Float32}) = (V ./ economy.V̲) |> central∇V |> expand∂
-
-
-G(Vₜ::Matrix{Float32}, t::Float32, X::Matrix{Float32}) = G!(copy(Vₜ), t, X)
-function G!(Vₜ::Matrix{Float32}, t::Float32, X::Matrix{Float32})
-    ∇Vₜ = ∇(Vₜ)
-    α = abatement(t, X, ∇Vₜ)
-    χ = consumption(economy.t₁, Vₜ, ∇Vₜ)
     w = drift(t, X, α, χ)
+    dir∇V = dir∇(V, w, Ω)
 
-    Vₜ .= Vₜ .+ ϵ .* f.(χ, X[[3], :] , V, Ref(economy))
-
-    for j in axes(Vₜ, 2)
-        Vₜ[:, j] .= Vₜ[:, j] .+ ϵ .* ∇Vₜ[:, j]'w[:, j]
-    end
-
-    Vₜ .= Vₜ .+ ∂²T(Vₜ) * (hogg.σ²ₜ / 2f0)
+    ∂ₜV .= f.(χ, X[:, :, :, 3] , V, Ref(economy)) .+ 
+        dir∇V[:, :, :, 4] .+ 
+        ∂²(1, V, Ω) * (hogg.σ²ₜ / 2f0)
     
-    return Vₜ
+    return ∂ₜV
 end
 
-Vₜ₋₁ = copy(Vₜ);
-@time G!(Vₜ₋₁, economy.t₁, X);
+const timegrid = range(0f0, 5f0; length = 101);
+function simulate(timegrid, W₀::FieldGrid; verbose = false)
+    h = step(timegrid)
+    τ = economy.t₁
+
+    Wpath = Array{Float32}(undef, size(W₀)..., length(timegrid) + 1)
+    Wpath[:, :, :, 1] .= W₀
+
+    verbose && println("Running backward simulation...")
+    
+    for (i, t) ∈ enumerate(timegrid)    
+        verbose && print("Iteration $i / $(length(timegrid))\r")
+        Wpath[:, :, :, i + 1] .= rkstep(Wpath[:, :, :, i], G, τ - t, X; h = h)
+    end
+    print("\n")
+
+    return Wpath
+end
+
+V̄ = @. X[:, :, :, 3]^2 - 1f5;
+Wpath = simulate(timegrid, V̄; verbose = true);
