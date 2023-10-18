10000e6c6992f81ad693d17ea80bbc70961edc45
NoFishLikeIan <andreatitton96@gmail.com>
2023-10-18 12:34:55 +0200

    🪐 parallelisation of maximum computation

--- src/tests/optimisation.jl
+++ src/tests/optimisation.jl
@@ -1,46 +1,86 @@
-using UnPack
-using StatsBase
-using LinearAlgebra
+using Distributed, SharedArrays
+addprocs(3);
+
+@everywhere begin
+    using UnPack 
+    
+    include("../../src/utils/grids.jl")
+    include("../../src/utils/derivatives.jl")
+    include("../../src/model/init.jl")
+end
+
+# -- Generate state cube
+n = 51
+const statedomain::Vector{Domain} = [
+    (hogg.T₀, hogg.T̄, n), 
+    (log(hogg.M₀), log(hogg.M̄), n), 
+    (log(economy.Y̲), log(economy.Ȳ), n)
+];
+const Ω = makeregulargrid(statedomain);
+const X = fromgridtoarray(Ω);
+
+# -- Generate action square
+m = 51
+const actiondomain::Vector{Domain} = [
+    (1f-3, 1f0 - 1f-3, m), (1f-3, 1f0 - 1f-3, m)
+]
+
+const Γ = makeregulargrid(actiondomain);
+const P = fromgridtoarray(Γ);
+
+# ---- Benchmarking
+V = -rand(Float32, size(X[:, :, :, 1])) .- 1f0;
+∇V = central∇(V, Ω);
+∂²V = ∂²(1, V, Ω);
+
+α = χ = 0.5f0;
+objective = similar(V);
+terminalpolicy = similar(V);
+idx = rand(CartesianIndices(V));
+
+t = 80f0;
+Xᵢ = @view X[idx, :];
+Vᵢ = @view V[idx];
+∇Vᵢ = @view ∇V[idx, :];
+∂²Vᵢ = @view ∂²V[idx];
 
 using BenchmarkTools
 
-include("../../src/utils/grids.jl")
-include("../../src/utils/derivatives.jl")
-include("../../src/model/init.jl")
+begin
+    println("HJB given control...")
+    @btime hjb($χ, $α, $t, $Xᵢ, $Vᵢ, $∇Vᵢ, $∂²Vᵢ);
 
-# -- Generate state cube
-statedomain::Vector{Domain} = [
-    (hogg.T₀, hogg.T̄, 101), 
-    (log(hogg.M₀), log(hogg.M̄), 61), 
-    (log(economy.Y̲), log(economy.Ȳ), 61)
-];
-Ω = makeregulargrid(statedomain);
-Nₛ = size(Ω);
-X = fromgridtoarray(Ω);
+    println("Objective functional given control...")
+    @btime objectivefunction($χ, $α, $t, $Xᵢ, $Vᵢ, $∇Vᵢ);
 
-# -- Generate action square
-m = 21
-actiondomain::Vector{Domain} = [
-    (1f-3, 1f0 - 1f-3, 11), (1f-3, 1f0 - 1f-3, 11)
-]
-
-Γ = makeregulargrid(actiondomain);
-Nₐ = size(Γ);
-P = fromgridtoarray(Γ);
-
-V = -rand(Float32, size(X[:, :, :, 1])) .- 1f0;
-∇V = central∇(V, Ω);
-
-t = 0f0;
-α = χ = 0.5f0;
-objective = similar(V);
-idx = rand(CartesianIndices(V));
-
-Xᵢ = @view X[idx, :]
-∇Vᵢ = @view ∇V[idx, :]
-Vᵢ = @view V[idx]
-t = 80f0
+    println("Optimal policy at a given point...")
+    @btime optimalpolicy($t, $Xᵢ, $Vᵢ, $∇Vᵢ, $Γ);
+    @btime terminalpolicyovergrid!(terminalpolicy, $X, $V, $∇V, $Γ);
 
 
-@btime control($χ, $α, $t, $Xᵢ, $Vᵢ, $∇Vᵢ);
-@btime objectivefunctional!(objective, $χ, $α, $t, $X, $V, $∇V);
+    # FIXME: the distributed value gives wrong values.
+    println("Optimal policy on the full grid...")
+    policy = SharedArray{Float32, 4}((size(V)..., 2));
+    policyovergrid!(policy, t, X, V, ∇V, Γ); @time policyovergrid!(policy, t, X, V, ∇V, Γ);
+
+    nondistpolicy = Array{Float32, 4}(undef, (size(V)..., 2));
+    policyovergrid!(nondistpolicy, t, X, V, ∇V, Γ); @time policyovergrid!(nondistpolicy, t, X, V, ∇V, Γ);
+    # @btime policyovergrid!(policy, $t, $X, $V, $∇V, $Γ); # FIXME: @btime not working for distributed
+end;
+
+if false
+    ∂ₜV = similar(V);
+    @btime Ḡ!(∂ₜV, $V, $X, $Ω, $Γ);
+
+    ∂ₜV = vec(∂ₜV);
+    const V₀ = (-1f0 .- rand(Float32, length(V)));
+    function Ḡ!(∂ₜV::Vector{Float32}, V::Vector{Float32}, p, t)
+        ∂ₜV .= vec(Ḡ(reshape(V, n, n, n), X, Ω, Γ)) .* (V .< 0)
+    end
+
+    @time Ḡ!(∂ₜV, V₀, [], 0f0);
+    prob = ODEProblem(Ḡ!, V₀, (0f0, 1f0));
+    sol = solve(prob)
+
+    terminalproblem = SteadyStateProblem(prob);
+end
--- src/utils/grids.jl
+++ src/utils/grids.jl
@@ -1,4 +1,5 @@
 using Base.Iterators: product
+using SharedArrays: SharedArray
 
 # Utilities for regular grid
 Domain = Tuple{Float32, Float32, Int};
@@ -7,7 +8,10 @@ ActionRegularGrid = NTuple{2, Vector{Float32}};
 StateRegularGrid = NTuple{3, Vector{Float32}};
 RegularGrid = Union{StateRegularGrid, ActionRegularGrid};
 FieldGrid = Array{Float32, 3};
+SharedFieldGrid = SharedArray{Float32, 3};
+
 VectorGrid = Array{Float32, 4};
+SharedVectorGrid = SharedArray{Float32, 4};
 
 steps(grid::RegularGrid) = ntuple(i -> grid[i][2] - grid[i][1], Val(3))
 Base.size(grid::RegularGrid) = prod(length, grid)
@@ -19,6 +23,7 @@ function makeregulargrid(domains::Vector{Domain})::RegularGrid
         length(domains)
     )
 end
+
 function fromgridtoarray(grid::ActionRegularGrid)::Array{Float32, 3}
     permutedims(
         collect(reinterpret(
@@ -34,9 +39,4 @@ function fromgridtoarray(grid::StateRegularGrid)::Array{Float32, 4}
         )),
         (2, 3, 4, 1)
     );
-end
-
-# Utilities for adaptive grid, mostly for action space
-function adaptgrid()
-
 end
