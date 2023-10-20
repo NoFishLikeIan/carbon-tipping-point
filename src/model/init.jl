91ccb11230c8d94106a1d1e434929cf1261396d3
NoFishLikeIan <andreatitton96@gmail.com>
2023-10-20 12:29:40 +0200

    🦉 fixed type instability

--- src/model/init.jl
+++ src/model/init.jl
@@ -2,8 +2,6 @@ using DotEnv, JLD2
 
 include("climate.jl")
 include("economy.jl")
-include("pdes.jl")
-include("terminalpde.jl")
 
 const env = DotEnv.config();
 const DATAPATH = get(env, "DATAPATH", "data/");
@@ -11,13 +9,14 @@ const calibration = JLD2.load(joinpath(DATAPATH, "calibration.jld2"))["calibrati
 
 # Initialise parameters
 const economy = Economy();
-const hogg = Hogg();
-const albedo = Albedo();
+const hogg = Hogg(); 
+const albedo = Albedo(); 
+
 
 # Construct the interpolation functions in t ∈ [0, 1]. A bit ugly hard coding but it works.
 const γparams = Float32.(calibration[:γparameters])
 γᵇ(t) = γ(t, γparams, economy.t₀)
-const Eᵇpath = calibration[:Eᵇ][3:end]
+const Eᵇpath = Float32.(calibration[:Eᵇ][3:end])
 
 function Eᵇ(t)
     dt = economy.t₁ / (length(Eᵇpath) - 1)
@@ -28,4 +27,20 @@ function Eᵇ(t)
     α = port / dt
 
     return Eᵇpath[idx] * (1 - α) + Eᵇpath[idx + 1] * α
+end
+
+function ε(t, M, α)
+    1f0 .- M .* (δₘ.(M, Ref(hogg)) .+ γᵇ(t) .- α) ./ (Gtonoverppm * Eᵇ(t))
+end
+
+drift(χ, α, t, X) = drift!(similar(X), χ, α, t, X)
+function drift!(w, χ, α, t, X)
+    T = @view X[:, :, :, 1]
+    m = @view X[:, :, :, 2]
+
+    w[:, :, :, 1] .= μ.(T, m, Ref(hogg), Ref(albedo))
+    w[:, :, :, 2] .= γᵇ.(t) .- α
+    w[:, :, :, 3] .= economy.ϱ .+ ϕ.(χ, A(t, economy), Ref(economy)) .- A(t, economy) .* β.(t, ε(t, exp.(m), α), Ref(economy)) .- δₖ.(T, Ref(economy), Ref(hogg))
+
+    return w
 end
