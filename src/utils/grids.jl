f0cadc093c358baad7d2ba12879d10403b45aeaa
NoFishLikeIan <andreatitton96@gmail.com>
2023-10-24 11:00:35 +0200

    🪐 fixed grids to accomodate multiple dimensions

--- src/utils/grids.jl
+++ src/utils/grids.jl
@@ -1,16 +1,10 @@
 using Base.Iterators: product
-using SharedArrays: SharedArray
 
 # Utilities for state grid, which have to be regular;
 RegularDomain = Tuple{Float32, Float32, Int};
-StateGrid = NTuple{3, Vector{Float32}};
+StateGrid{N} = NTuple{N, Vector{Float32}};
 
-FieldGrid = Array{Float32, 3};
-SFieldGrid = Array{Float32, 3};
-VectorGrid = Array{Float32, 4};
-SVectorGrid = SharedArray{Float32, 4};
-
-steps(grid) = ntuple(i -> grid[i][2] - grid[i][1], Val(3))
+steps(grid::StateGrid{N}) where N = ntuple(i -> grid[i][2] - grid[i][1], Val(N))
 Base.size(grid) = prod(length, grid)
 
 function makegrid(domains::Vector{RegularDomain})
@@ -22,18 +16,14 @@ function makegrid(domains::Vector{RegularDomain})
     )
 end
 
-function fromgridtoarray(grid)::VectorGrid
+function fromgridtoarray(grid)
+    
+    shifted = collect(1:length(grid)) .+ 1
+
     permutedims(
-        collect(reinterpret(
-            reshape, Float32, collect(Iterators.product(grid...))
-        )),
-        (2, 3, 4, 1) # Re-orders the columns
+        reinterpret(
+            reshape, Float32, Iterators.product(grid...) |> collect
+        ) |> collect,
+        (shifted..., 1) # Re-orders the columns
     );
-end
-
-# Utilities for action grid, relying on LatinHypercubeSampling
-LatinDomain = Tuple{Float32, Float32}
-
-function makegrid(domains::Vector{LatinDomain}, n)
-    Float32.(scaleLHC(randomLHC(n, 2), domains))
 end
