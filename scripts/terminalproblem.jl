e46042501c9b607b4c9cca5025c20b9b9ab1d2f8
NoFishLikeIan <andreatitton96@gmail.com>
2023-10-26 16:43:34 +0200

    🦉 solution to terminal problem

--- scripts/terminalproblem.jl
+++ scripts/terminalproblem.jl
@@ -1,11 +1,8 @@
 using Revise
 
 using Model, Utils
-
-using NLsolve, PreallocationTools
-using Plots
-
-using FiniteDiff
+using DifferentialEquations, NonlinearSolve
+using PreallocationTools
 
 using DotEnv, JLD2
 
@@ -17,31 +14,29 @@ const albedo = Model.Albedo();
 
 const instance = (economy, hogg, albedo);
 
-const domain = [(hogg.T₀, hogg.T̄, 51), (log(economy.Y̲), log(economy.Ȳ), 51)];
+const domain = [(hogg.T₀, hogg.T̄, 101), (log(economy.Y̲), log(economy.Ȳ), 51)];
 const Ω = Utils.makegrid(domain);
 const X = Utils.fromgridtoarray(Ω);
 
-Vguess = [ -2f0 + (exp(y) / economy.Y₀)^2 - (T / hogg.Tᵖ)^3 for T ∈ Ω[1], y ∈ Ω[2] ];
-
-const tmp = DiffCache(Array{Float32}(undef, length.(Ω)..., 4));
-terminalsteadystate(V̂) = terminalsteadystate!(similar(V̂), V̂);
-function terminalsteadystate!(∂ₜV, V̂)
-    V = -exp.(V̂)
-    vec(terminalG!(∂ₜV, get_tmp(tmp, V), X, V, Ω, instance))
+const tmpcache = DiffCache(Array{Float32}(undef, length.(Ω)..., 4));
+terminalsteadystate(Vₜ, p) = terminalsteadystate!(similar(Vₜ), Vₜ, p, 0f0);
+function terminalsteadystate!(∂ₜV, Vₜ, p, t)
+    tmp = get_tmp(first(p), Vₜ)
+    terminalG!(∂ₜV, tmp, X, Vₜ, Ω, instance)
 end
 
-V̂₀ = log.(-Vguess);
-∂ₜV₀ = terminalsteadystate(V̂₀);
+V₀ = [ -2f0 + (exp(y) / economy.Y₀)^2 - (T / hogg.Tᵖ)^3 for T ∈ Ω[1], y ∈ Ω[2] ];
+∂ₜV₀ = similar(V₀);
 
-println("Non-linear solver...")
-sol = nlsolve(terminalsteadystate!, V̂₀; show_trace = true);
+params = (tmpcache, );
+prob = ODEProblem(terminalsteadystate!, V₀, (0f0, 20f0), params);
+sol = solve(prob);
 
-V̄ = -exp.(sol.zero)
-∂ₜV̄ = terminalG(X, V̄, Ω, instance)
+V̄ = sol[end];
+∂ₜV̄ = terminalG(X, V̄, Ω, instance);
 println("Sup. norm = $(maximum(abs.(∂ₜV̄)))")
-
 
 println("Saving solution...")
 env = DotEnv.config()
 DATAPATH = get(env, "DATAPATH", "data/") 
-@save joinpath(DATAPATH, "terminal.jld2") V̄  
+@save joinpath(DATAPATH, "terminal.jld2") V̄
--- src/timederivative.jl
+++ src/timederivative.jl
@@ -1,3 +1,7 @@
+using Model: terminalpolicyovergrid!, ydrift!, hjbterminal
+
+using Utils: central∂!, dir∂!, ∂²!
+
 function G(t, X, V, Ω, instance::Model.ModelInstance, calibration::Model.Calibration)
     G!(
         similar(V), similar(V, size(V)..., 4),
@@ -7,13 +11,14 @@ end
 "Computes G! by modifying (∂ₜV, ∇V, policy, w)"
 function G!(∂ₜV, tmp, t, X, V, Ω, instance::Model.ModelInstance, calibration::Model.Calibration)
     economy = first(instance)
+    # FIXME: Using `tmp`
 
     central∇!(∇V, V, Ω)
     policyovergrid!(policy, t, X, V, ∇V, instance, calibration);
     drift!(policy, α, t, X, instance, calibration);
     dir∇!(∇V, V, w, Ω);
 
-    ∂ₜV .= f.(χ, X[:, :, :, 3], V, Ref(economy)) + ∇V[:, :, :, 4] .+ ∂²(V, Ω; dim = 1) .* hogg.σ²ₜ / 2f0
+    ∂ₜV .= f.(χ, X[:, :, :, 3], V, economy) + ∇V[:, :, :, 4] .+ ∂²(V, Ω; dim = 1) .* hogg.σ²ₜ / 2f0
 
     return ∂ₜV
 end
@@ -33,18 +38,20 @@ function terminalG!(∂ₜV, tmp, X, V, Ω, instance::Model.ModelInstance)
     ∂²TV = @view tmp[:, :, 2]
     policy = @view tmp[:, :, 3]
     w = @view tmp[:, :, 4]
+
     T = @view X[:, :, 1]
-    y = @view X[:, :, 2]
 
-    Utils.central∂!(∂yV, V, Ω; direction = 2);
-    Model.terminalpolicyovergrid!(policy, X, V, ∂yV, instance);
-    Model.ydrift!(w, policy, T, instance);
-    Utils.dir∂!(∂yV, V, w, Ω; direction = 2);
-    Utils.∂²!(∂²TV, V, Ω; dim = 1)
-
+    central∂!(∂yV, V, Ω; direction = 2);
+    terminalpolicyovergrid!(policy, X, V, ∂yV, instance);
+    ydrift!(w, policy, T, instance);
+    dir∂!(∂yV, V, w, Ω; direction = 2);
+    ∂²!(∂²TV, V, Ω; dim = 1)
 
     for idx in CartesianIndices(∂ₜV)
-        ∂ₜV[idx] = Model.hjbterminal(policy[idx], X[idx, :], V[idx], ∂yV[idx], ∂²TV[idx], instance)
+        ∂ₜV[idx] = ifelse(V[idx] < 0f0,
+            hjbterminal(policy[idx], X[idx, :], V[idx], ∂yV[idx], ∂²TV[idx], instance),
+            0f0
+        )
         
     end
 
