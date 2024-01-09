5db42627062ba0aa035e42864493b30b89dde800
NoFishLikeIan <andreatitton96@gmail.com>
2024-01-09 09:20:54 +0100

    🧹 extended type definition of interpolations

--- src/Model/src/grid/interpolations.jl
+++ src/Model/src/grid/interpolations.jl
@@ -2,7 +2,7 @@ function interpolateovergrid(fromgrid::RegularGrid, V::AbstractArray, togrid::Re
     interpolateovergrid(fromgrid, V, togrid.X)
 end
 
-function interpolateovergrid(grid::RegularGrid, V::AbstractArray{Float64, 3}, xs::AbstractArray{Point})
+function interpolateovergrid(grid::RegularGrid, V::AbstractArray{Float64, 3}, xs::AbstractArray{Point, M})::AbstractArray{Float64, M} where M
     N = size(grid, 1)
     knots = ntuple(i -> range(grid.domains[i][1], grid.domains[i][2], length = N), 3)
     itp = scale(interpolate(V, BSpline(Linear())), knots)    
@@ -10,7 +10,7 @@ function interpolateovergrid(grid::RegularGrid, V::AbstractArray{Float64, 3}, xs
     [itp(x.T, x.m, x.y) for x ∈ xs]
 end
 
-function interpolateovergrid(grid::RegularGrid, P::AbstractArray{Policy, 3}, xs::AbstractArray{Point})
+function interpolateovergrid(grid::RegularGrid, P::AbstractArray{Policy, 3}, xs::AbstractArray{Point, M})::AbstractArray{Policy, M} where M
     N = size(grid, 1)
     knots = ntuple(i -> range(grid.domains[i][1], grid.domains[i][2], length = N), 3)
     itpχ = scale(interpolate(first.(P), BSpline(Linear())), knots)
