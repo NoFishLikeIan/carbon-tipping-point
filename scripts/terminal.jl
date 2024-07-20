466fcd83835e7f420835ed45c327a14ceeb079bf
NoFishLikeIan <andreatitton96@gmail.com>
2024-07-20 15:42:02 +0200

    🧹 reverted back terminal to old version

--- scripts/terminal.jl
+++ scripts/terminal.jl
@@ -9,47 +9,132 @@ using Printf: @printf, @sprintf
 
 include("utils/saving.jl")
 
-function cost(Kᵢ, Tᵢ, Δt, χ, model::AbstractModel{GrowthDamages, P}) where P
-    δ = outputdiscount(Tᵢ, Δt, χ, model)
+# TODO: The two functions below differe only in expected utility. Factor that out.
 
-    g(χ, δ * Kᵢ, Δt, model.preferences)
-end
+"Computes the Jacobi iteration for the terminal problem, F̄."
+function terminaljacobi!(F̄::AbstractMatrix{Float64}, policy::AbstractMatrix{Float64}, model::ModelInstance, G::RegularGrid; indices = CartesianIndices(G))
 
-function cost(Kᵢ, Tᵢ, Δt, χ, model::AbstractModel{LevelDamages, P}) where P
-    δ = outputdiscount(Tᵢ, Δt, χ, model)
-    damage = d(Tᵢ, model.damages, model.hogg)
-
-    g(damage * χ, δ * Kᵢ, Δt, model.preferences)
-end
-
-""
-function steadystatestep!(K, policy, model::AbstractModel, G; indices = CartesianIndices(K), Δt = 0.05)    
+    L, R = extrema(CartesianIndices(G))
+    
     for idx in indices
-        Kᵢ = K[idx]
-        Tᵢ = G.X[idx].T
-        objective = @closure χ -> cost(Kᵢ, Tᵢ, Δt, χ, model)
+        @unpack θ, ρ, ψ = model.preferences
+        σₜ² = (model.hogg.σₜ / (model.hogg.ϵ * G.Δ.T))^2
+        σₘ² = (model.hogg.σₘ / G.Δ.m)^2
 
-        Kᵢ′, χ = gssmin(objective, 0., 1.)
-        K[idx] = Kᵢ′
+        Xᵢ = G.X[idx]
+        dT = μ(Xᵢ.T, Xᵢ.m, model.hogg, model.albedo) / (model.hogg.ϵ * G.Δ.T)
+    
+        # Neighbouring nodes
+        # -- Temperature
+        FᵢT₊ = F̄[min(idx + I[1], R)]
+        FᵢT₋ = F̄[max(idx - I[1], L)]
+
+        Fᵢm₊ = F̄[min(idx + I[2], R)]
+        Fᵢm₋ = F̄[max(idx - I[2], L)]
+    
+        dTF = G.h * abs(dT) * ifelse(dT > 0, FᵢT₊, FᵢT₋)+ σₜ² * (FᵢT₊ + FᵢT₋) / 2
+        dmF = σₘ² * (Fᵢm₊ + Fᵢm₋) / 2
+
+        Q = σₘ² + σₜ² + G.h * abs(dT)
+        F′ = (dTF + dmF) / Q
+
+        σₖ² = model.economy.σₖ^2
+        damage = d(Xᵢ.T, model.damages, model.hogg)
+        growth = model.economy.ϱ - model.economy.δₖᵖ
+
+        Δt = G.h^2 / Q
+        
+        costs = @closure χ -> begin
+            investment = ϕ(model.economy.τ, χ, model.economy)
+            μy = growth + investment - damage
+            δy = max(1 + (1 - θ) * (μy - θ * σₖ²) * Δt, 0.)
+
+            return g(χ, δy * F′, Δt, model.preferences)
+        end
+
+        # Optimal control
+        objmin, χ = gssmin(costs, 0., 1.; tol = 1e-6)
+        
         policy[idx] = χ
+        F̄[idx] = objmin
     end
+
+    return F̄, policy
+end
+function terminaljacobi!(F̄::AbstractMatrix{Float64}, policy::AbstractMatrix{Float64}, model::ModelBenchmark, G::RegularGrid; indices = CartesianIndices(G))
+
+    L, R = extrema(CartesianIndices(G))
+    
+    for idx in indices
+        @unpack θ, ρ, ψ = model.preferences
+        σₜ² = (model.hogg.σₜ / (model.hogg.ϵ * G.Δ.T))^2
+        σₘ² = (model.hogg.σₘ / G.Δ.m)^2
+
+        Xᵢ = G.X[idx]
+        dT = μ(Xᵢ.T, Xᵢ.m, model.hogg) / (model.hogg.ϵ * G.Δ.T)
+
+        # Jump
+        πᵢ = intensity(Xᵢ.T, model.hogg, model.jump)
+        qᵢ = increase(Xᵢ.T, model.hogg, model.jump)
+
+        steps = floor(Int, div(qᵢ, G.Δ.T * G.h))
+        weight = qᵢ / (G.Δ.T * G.h)
+
+        Fʲ = F̄[min(idx + steps * I[1], R)] * (1 - weight) + F̄[min(idx + (steps + 1) * I[1], R)] * weight
+
+        # Neighbouring nodes
+        # -- Temperature
+        FᵢT₊ = F̄[min(idx + I[1], R)]
+        FᵢT₋ = F̄[max(idx - I[1], L)]
+
+        Fᵢm₊ = F̄[min(idx + I[2], R)]
+        Fᵢm₋ = F̄[max(idx - I[2], L)]
+    
+        dTF = G.h * abs(dT) * ifelse(dT > 0, FᵢT₊, FᵢT₋)+ σₜ² * (FᵢT₊ + FᵢT₋) / 2
+        dmF = σₘ² * (Fᵢm₊ + Fᵢm₋) / 2
+
+        Q = σₘ² + σₜ² + G.h * abs(dT)
+        Δt = G.h^2 / Q
+        Fᵈ = (dTF + dmF) / Q
+        F′ = Fᵈ + πᵢ * Δt * (Fʲ - Fᵈ)
+
+        σₖ² = model.economy.σₖ^2
+        damage = d(Xᵢ.T, model.damages, model.hogg)
+        growth = model.economy.ϱ - model.economy.δₖᵖ
+        
+        costs = @closure χ -> begin
+            investment = ϕ(model.economy.τ, χ, model.economy)
+            μy = growth + investment - damage
+            δy = max(1 + (1 - θ) * (μy - θ * σₖ²) * Δt, 0.)
+
+            return g(χ, δy * F′, Δt, model.preferences)
+        end
+
+        # Optimal control
+        objmin, χ = gssmin(costs, 0., 1.; tol = 1e-6)
+        
+        policy[idx] = χ
+        F̄[idx] = objmin
+    end
+
+    return F̄, policy
 end
 
-function vfi(K₀, model, G; tol = 1e-3, maxiter = 10_000, verbose = false, indices = CartesianIndices(K₀), alternate = false, Δt = 0.05)
-    Kᵢ = copy(K₀)
-    Kᵢ₊₁ = copy(K₀)
+function vfi(F₀::AbstractMatrix{Float64}, model, G::RegularGrid; tol = 1e-3, maxiter = 10_000, verbose = false, indices = CartesianIndices(G), alternate = false)
+    pᵢ = 0.5 .* ones(size(G))
+    pᵢ₊₁ = copy(pᵢ)
 
-    pᵢ = similar(Kᵢ)
-    pᵢ₊₁ = similar(Kᵢ)
+    Fᵢ = copy(F₀)
+    Fᵢ₊₁ = copy(F₀)
 
     verbose && println("Starting iterations...")
     for iter in 1:maxiter
         iterindices = ifelse(alternate && isodd(iter), indices, reverse(indices))
 
-        steadystatestep!(Kᵢ₊₁, pᵢ₊₁, model, G; indices = iterindices, Δt = Δt)
+        terminaljacobi!(Fᵢ₊₁, pᵢ₊₁, model, G; indices = iterindices)
 
-        ε = maximum(abs.((Kᵢ₊₁ .- Kᵢ) ./ Kᵢ))
-        α = maximum(abs.((pᵢ₊₁ .- pᵢ) ./ pᵢ))
+        ε = maximum(abs.((Fᵢ₊₁ .- Fᵢ)))
+        α = maximum(abs.((pᵢ₊₁ .- pᵢ)))
 
         if verbose && (!alternate || isodd(iter))
             @printf("Iteration %i / %i, ε = %.8f and α = %.8f...\r", iter, maxiter, ε, α)
@@ -57,29 +142,29 @@ function vfi(K₀, model, G; tol = 1e-3, maxiter = 10_000, verbose = false, indi
 
         if ε < tol
             verbose && @printf("Converged in %i iterations, ε = %.8f and α = %.8f.\n", iter, ε, α)
-            return Kᵢ₊₁, pᵢ₊₁
+            return Fᵢ₊₁, pᵢ₊₁
         end
         
-        Kᵢ .= Kᵢ₊₁
+        Fᵢ .= Fᵢ₊₁
         pᵢ .= pᵢ₊₁
     end
 
     @warn "Convergence failed."
-    return Kᵢ₊₁, pᵢ₊₁
+    return Fᵢ₊₁, pᵢ₊₁
 end
 
-function computeterminal(model, G; verbose = true, withsave = true, datapath = "data", iterkwargs...)    
+function computeterminal(model, G::RegularGrid; verbose = true, withsave = true, datapath = "data", iterkwargs...)    
 
-    K₀ = ones(size(G))
-    K, policy = vfi(K₀, model, G; verbose, iterkwargs...)
+    F₀ = ones(size(G))
+    F̄, policy = vfi(F₀, model, G; verbose, iterkwargs...)
     
     if withsave
-    
+        folder = typeof(model) <: ModelInstance ? "albedo" : "jump"
         filename = makefilename(model, G)
-        savepath = joinpath(datapath, "terminal", filename)
+        savepath = joinpath(datapath, folder, "terminal", filename)
         println("Saving solution into $savepath...")
-        jldsave(savepath; K, policy)
+        jldsave(savepath; F̄, policy)
     end
 
-    return K, policy
+    return F̄, policy
 end
