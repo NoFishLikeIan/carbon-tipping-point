e85f9bf2d13896792b020def1be4a23f5129c6a2
NoFishLikeIan <andreatitton96@gmail.com>
2024-07-24 14:43:03 +0200

    🦉 docs on stochastic discount factor

--- scripts/run.jl
+++ scripts/run.jl
@@ -1,42 +1,35 @@
 include("terminal.jl")
 include("backward.jl")
 
-ΔΛ = [0.06, 0.08];
+ΔΛ = [0., 0.06, 0.08];
 N = 51;
 VERBOSE = true
 
 # Construct model
 preferences = EpsteinZin();
-jump = Jump()
 calibration = load_object(joinpath(DATAPATH, "calibration.jld2"));
-
 economy = Economy()
 hogg = Hogg()
-damages = GrowthDamages()
+damages = [LevelDamages(), GrowthDamages()]
 
-# Construct Grid
-Tdomain = hogg.T₀ .+ (0., 9.)
-mmin = mstable(Tdomain[1] - 0.75, hogg, Albedo())
-mmax = mstable(Tdomain[2], hogg, Albedo())
+# Terminal simulation
+for d in damages
+    VERBOSE && println("Solving for damages = $d...")
+    for Δλ ∈ ΔΛ
+        VERBOSE && println("Solving albedo model Δλ = $Δλ")
 
-mdomain = (mmin, mmax)
-domains = [Tdomain, mdomain]
+        λ₂ = Albedo().λ₁ - Δλ
+        albedo = Albedo(λ₂ = λ₂)
+        model = TippingModel(albedo, preferences, d, economy, hogg, calibration)
 
-G = RegularGrid(domains, N);
+        G = constructdefaultgrid(N, model)
 
-# Jump process
-@printf("Jump simulation\n")
-jumpmodel = ModelBenchmark(preferences, economy, damages, hogg, jump, calibration)
-computeterminal(jumpmodel, G; verbose = VERBOSE, withsave = true, datapath = DATAPATH, tol = 1e-4, maxiter = 20_000, alternate = true)
-computevalue(jumpmodel, G; docache = true, verbose = VERBOSE)
+        computeterminal(model, G; verbose = VERBOSE, datapath = DATAPATH, alternate = true, tol = 1e-3)
+    end
 
-for Δλ ∈ ΔΛ
-    @printf("Albedo simulation with Δλ = %.2f\n", Δλ)
-    # Construct model
-    albedo = Albedo(λ₂ = Albedo().λ₁ - Δλ)
-    hogg = calibrateHogg(albedo)
-    model = ModelInstance(preferences, economy, damages, hogg, albedo, calibration)
-    
-    computeterminal(model, G; verbose = VERBOSE, withsave = true, datapath = DATAPATH, alternate = true, tol = 1e-4, maxiter = 20_000)
-    computevalue(model, G; verbose = VERBOSE, docache = true)
+    jumpmodel = JumpModel(Jump(), preferences, d, economy, hogg, calibration)
+
+    G = constructdefaultgrid(N, jumpmodel)
+
+    computeterminal(model, G; verbose = VERBOSE, datapath = DATAPATH, alternate = true, tol = 1e-3)
 end
