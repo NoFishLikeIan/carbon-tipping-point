f7064a5c6855fa9371e89f2b77139e3244c0f9a9
NoFishLikeIan <andreatitton96@gmail.com>
2024-09-16 17:12:12 +0200

    🪐 updated terminal problem.

--- scripts/runbenchmark.jl
+++ scripts/runbenchmark.jl
@@ -14,61 +14,51 @@ addprocs(procs; exeflags="--project") # A bit sad that I have to do this
 
 include("utils/saving.jl")
 
-# Setup environment via .env
-env = config()
+include("arguments.jl") # Import argument parser
 
-DATAPATH = get(env, "DATAPATH", "data")
-SIMPATH = get(env, "SIMULATIONPATH", "simulation/planner")
-ALLOWNEGATIVE = getbool(env, "ALLOWNEGATIVE", false)
+parsedargs = ArgParse.parse_args(argtable)
 
-datapath = joinpath(DATAPATH, SIMPATH, ALLOWNEGATIVE ? "negative" : "")
+@unpack overwrite, datapath, simulationpath, N, cachestep, tol, verbose, stopat, procs = parsedargs
 
-N = getnumber(env, "N", 31; type = Int)
-VERBOSE = getbool(env, "VERBOSE", false)
-RUNTERMINAL = getbool(env, "RUNTERMINAL", false)
-RUNBACKWARDS = getbool(env, "RUNBACKWARDS", false)
-OVERWRITE = getbool(env, "OVERWRITE", false)
-TOL = getnumber(env, "TOL", 1e-3)
-TSTOP = getnumber(env, "TSTOP", 0.)
-CACHESTEP = getnumber(env, "CACHESTEP", 1 / 4)
-
-OVERWRITE && @warn "Running in overwrite mode!"
+overwrite && (verbose ≥ 1) && @warn "Running in overwrite mode!"
 
 # Distributed processing
 using Distributed: nprocs, addprocs
-ADDPROCS = getnumber(env, "ADDPROCS", 0; type = Int)
-addprocs(ADDPROCS; exeflags="--project") # A bit sad that I have to do this
+addprocs(procs; exeflags="--project") # A bit sad that I have to do this
 
-VERBOSE && "Running with $(nprocs()) processor..."
+(verbose ≥ 1) && "Running with $(nprocs()) processor..."
 
+include("utils/saving.jl")
 include("markov/terminal.jl")
 include("markov/backward.jl")
 
 # Construct model
-calibration = load_object(joinpath(DATAPATH, "calibration.jld2"));
+calibrationdirectory = joinpath(datapath, "calibration.jld2")
+calibration = load_object(calibrationdirectory);
+
+preferences = EpsteinZin();
+economy = Economy()
 hogg = Hogg()
 damages = GrowthDamages()
 jump = Jump()
+
+model = JumpModel(jump, hogg, preferences, damages, economy, calibration)
 
 # Construct Grid
 Tdomain = hogg.Tᵖ .+ (0., 9.);
 mdomain = mstable.(Tdomain, hogg)
 G = RegularGrid([Tdomain, mdomain], N)
 
-for ψ ∈ Ψ, θ ∈ Θ, ϱ ∈ Ρ, ωᵣ ∈ Ωᵣ
-    preferences = EpsteinZin(θ = θ, ψ = ψ);
-    economy = Economy(ϱ = ϱ, ωᵣ = ωᵣ)
-    jumpmodel = JumpModel(jump, hogg, preferences, damages, economy, calibration)
+for allownegative in [false, true]
+    (verbose ≥ 1) && println("Solving model $(allownegative ? "with" : "without") negative emission...")
 
+    outdir = joinpath(datapath, simulationpath, 
+    allownegative ? "negative" : "constrained")
 
-    VERBOSE && println("\nSolving jump model $(ALLOWNEGATIVE ? "with" : "without") negative emission...")
-    if RUNTERMINAL
-        Gterminal = terminalgrid(N, jumpmodel)
-        computeterminal(jumpmodel, Gterminal; verbose = VERBOSE, datapath = datapath, alternate = true, tol = TOL, overwrite = OVERWRITE)
-    end
+    (verbose ≥ 1) && println("Running terminal...")
+    Gterminal = terminalgrid(N, model)
+    computeterminal(model, Gterminal; verbose, outdir, alternate = true, tol, overwrite)
 
-    if RUNBACKWARDS
-        VERBOSE && println("Running backward simulation...")
-        computebackward(jumpmodel, G; verbose = VERBOSE, datapath = datapath, overwrite = OVERWRITE, tstop = TSTOP, cachestep = CACHESTEP)
-    end
+    (verbose ≥ 1) && println("Running backward...")
+    computebackward(model, G; verbose, outdir, overwrite, tstop = stopat, cachestep, allownegative)
 end
