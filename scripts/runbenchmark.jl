dad18104f9041034dd1b7498d4f82eadae37bdb1
NoFishLikeIan <andreatitton96@gmail.com>
2024-09-18 12:12:42 +0200

    🪐 updated simulation option

--- scripts/runbenchmark.jl
+++ scripts/runbenchmark.jl
@@ -16,12 +16,32 @@ include("utils/saving.jl")
 include("markov/terminal.jl")
 include("markov/backward.jl")
 
-# Construct model
-calibrationdirectory = joinpath(datapath, "calibration.jld2")
-calibration = load_object(calibrationdirectory);
+env = DotEnv.config()
+DATAPATH = get(env, "DATAPATH", "data")
+SIMPATH = get(env, "SIMULATIONPATH", "simulation/planner")
+ALLOWNEGATIVE = getbool(env, "ALLOWNEGATIVE", false)
 
-preferences = EpsteinZin();
-economy = Economy()
+datapath = joinpath(DATAPATH, SIMPATH, ALLOWNEGATIVE ? "negative" : "")
+
+N = getnumber(env, "N", 31; type = Int)
+VERBOSE = getbool(env, "VERBOSE", false)
+RUNTERMINAL = getbool(env, "RUNTERMINAL", false)
+RUNBACKWARDS = getbool(env, "RUNBACKWARDS", false)
+OVERWRITE = getbool(env, "OVERWRITE", false)
+TOL = getnumber(env, "TOL", 1e-3)
+TSTOP = getnumber(env, "TSTOP", 0.)
+CACHESTEP = getnumber(env, "CACHESTEP", 1 / 4)
+
+OVERWRITE && @warn "Running in overwrite mode!"
+
+# Parameters
+Ψ = [0.75, 1.5]
+Θ = [2., 10.]
+Ρ = [1e-7, 1e-3]
+Ωᵣ = [0., 0.017558043747351086]
+
+# Construct model
+calibration = load_object(joinpath(DATAPATH, "calibration.jld2"));
 hogg = Hogg()
 damages = GrowthDamages()
 jump = Jump()
@@ -33,16 +53,20 @@ Tdomain = hogg.Tᵖ .+ (0., 9.);
 mdomain = mstable.(Tdomain, hogg)
 G = RegularGrid([Tdomain, mdomain], N)
 
-for allownegative in [false, true]
-    (verbose ≥ 1) && println("Solving model $(allownegative ? "with" : "without") negative emission...")
+for Tᶜ ∈ thresholds, ψ ∈ Ψ, θ ∈ Θ, ϱ ∈ Ρ, ωᵣ ∈ Ωᵣ
+    preferences = EpsteinZin(θ = θ, ψ = ψ);
+    economy = Economy(ϱ = ϱ, ωᵣ = ωᵣ)
+    jumpmodel = JumpModel(jump, hogg, preferences, damages, economy, calibration)
 
-    outdir = joinpath(datapath, simulationpath, 
-    allownegative ? "negative" : "constrained")
 
-    (verbose ≥ 1) && println("Running terminal...")
-    Gterminal = terminalgrid(N, model)
-    computeterminal(model, Gterminal; verbose, outdir, alternate = true, tol, overwrite)
+    VERBOSE && println("\nSolving jump model $(ALLOWNEGATIVE ? "with" : "without") negative emission...")
+    if RUNTERMINAL
+        Gterminal = terminalgrid(N, jumpmodel)
+        computeterminal(jumpmodel, Gterminal; verbose = VERBOSE, datapath = datapath, alternate = true, tol = TOL, overwrite = OVERWRITE)
+    end
 
-    (verbose ≥ 1) && println("Running backward...")
-    computebackward(model, G; verbose, outdir, overwrite, tstop = stopat, cachestep, allownegative)
+    if RUNBACKWARDS
+        VERBOSE && println("Running backward simulation...")
+        computebackward(jumpmodel, G; verbose = VERBOSE, datapath = datapath, overwrite = OVERWRITE, tstop = TSTOP, cachestep = CACHESTEP)
+    end
 end
