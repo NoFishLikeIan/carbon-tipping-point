630510e3922d1e5598ae9fa929119cf6b661890e
NoFishLikeIan <andreatitton96@gmail.com>
2024-09-24 17:27:31 +0200

    🧪 test job for optimal

--- jobs/experiments/makeparameters.jl
+++ jobs/experiments/makeparameters.jl
@@ -1,7 +1,9 @@
 using JSON
 include("../../scripts/utils/saving.jl")
 
-inputfiles = listfiles("data/simulation-medium");
+inputdir = isempty(ARGS) ? "data/simulation-medium" : first(ARGS);
+inputfiles = replace.(listfiles(inputdir), "data/" => "");
+
 obj = Dict("inputfiles" => inputfiles);
 
 open("jobs/experiments/parameters.json", "w") do f
