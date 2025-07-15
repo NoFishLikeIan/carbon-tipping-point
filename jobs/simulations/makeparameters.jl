f5e0fc4b510d1af366451dec9daaa798fa36ae83
NoFishLikeIan <andreatitton96@gmail.com>
2025-07-15 10:34:10 +0200

    🦉 updated parameters to include negative emissions

--- jobs/simulations/makeparameters.jl
+++ jobs/simulations/makeparameters.jl
@@ -4,16 +4,17 @@ rras = [2.0, 10.0];
 eiss = [0.75, 1.5];
 thresholds = [1.5, 2.5, 3.5];
 withleveldamages = [false];
+withnegatives = [false, true];
 
 tippingparameters = Dict{String, Union{Float64, Bool}}[];
 benchmarkparameters = Dict{String, Union{Float64, Bool}}[];
 
 
-for rra in rras, eis in eiss, leveldamages in withleveldamages
-    push!(benchmarkparameters, Dict("rra" => rra, "eis" => eis, "leveldamages" => leveldamages))
+for rra in rras, eis in eiss, leveldamages in withleveldamages, withnegative in withnegatives
+    push!(benchmarkparameters, Dict("rra" => rra, "eis" => eis, "leveldamages" => leveldamages, "withnegative" => withnegative))
 
     for threshold in thresholds
-        push!(tippingparameters, Dict("threshold" => threshold, "rra" => rra, "eis" => eis, "leveldamages" => leveldamages))
+        push!(tippingparameters, Dict("threshold" => threshold, "rra" => rra, "eis" => eis, "leveldamages" => leveldamages, "withnegative" => withnegative))
     end
 end
 
