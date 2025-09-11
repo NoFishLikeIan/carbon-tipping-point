a9e99c4afc636e37503c9de6eb9f3bace4ac8f84
NoFishLikeIan <andreatitton96@gmail.com>
2025-09-11 09:37:08 +0200

    ✨ generalised mac function

--- scripts/markov/chain.jl
+++ scripts/markov/chain.jl
@@ -1,23 +1,47 @@
-function αopt(t, Xᵢ::Point, ∂ₘH, model::M, calibration::Calibration) where {S, M <: UnitElasticityModel{S}}
-    -∂ₘH * ᾱ(t, Xᵢ, model, c
-alibration)^2 / (A(t, economy) * ω(t, model.economy) * (1 - model.preferences.θ))
+function αopt(t, Xᵢ::Point, ∂ₘH, model::M, calibration::Calibration) where {S, M <: UnitIAM{S}}
+    @unpack economy, preferences = model
+
+    if ∂ₘH ≤ 0 return zero(S) end
+
+    num = ∂ₘH * ᾱ(t, Xᵢ, model, calibration)^economy.abatement.b
+    den = economy.abatement.b * A(t, economy.investments) * ω(t, economy.abatement) * (preferences.θ - 1)
+
+    return (num / den)^inv(economy.abatement.b - 1)
+end
+function upperbound(t, Xᵢ, model, calibration, withnegative)
+    ifelse(withnegative, 1.5, 1.) * ᾱ(t, Xᵢ, model, calibration)
+end
+function nstencil(::GR) where {N₁, N₂, GR <: AbstractGrid{N₁, N₂}}
+    9 * N₁ * N₂ - 2N₁ - 2N₂
+end
+function makestencil(::GR) where {N₁, N₂, S, GR <: AbstractGrid{N₁, N₂, S}}
+    n = nstencil(G)
+    idx = Vector{Int}(undef, n)
+    jdx = Vector{Int}(undef, n)
+    data = Vector{S}(undef, n)
+
+    return (idx, jdx, data)
 end
 
-function upperbound(t, Xᵢ, model, ca economy.abatement.b *libration, withnegative)
-    ifelse(withnegative, 1.5, 1) * ᾱ(t, Xᵢ, model, calibration)
-end
-
+StencilData{S} = Tuple{Vector{Int}, Vector{Int}, Vector{S}}
 "Constructs upwind-downwind scheme discretiser `(ρ + Δt⁻¹)I - L - B(Hⁿ)` and updates policy accordingly."
-function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR, calibration::Calibration, withnegative) where {N₁, N₂, S, M <: UnitElasticityModel{S}, GR <: AbstractGrid{N₁, N₂, S}}
-    σₜ² = (model.hogg.σₜ / model.hogg.ϵ)^2 / 2
-    σₘ² = model.hogg.σₘ^2 / 2
+function constructA!(V, Δt⁻¹, model, G::GR, calibration, withnegative) where {N₁, N₂, S, GR <: AbstractGrid{N₁, N₂, S}}
+    constructA!(makestencil(G), V, Δt⁻¹, model, G, calibration, withnegative)
+end
+function constructA!(stencil::StencilData{S}, V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR, calibration::Calibration, withnegative) where {N₁, N₂, S, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
+    (idx, jdx, data) = stencil
+
+    σₜ² = (model.climate.hogg.σₜ / model.climate.hogg.ϵ)^2 / 2
+    σₘ² = model.climate.hogg.σₘ^2 / 2
 
     t = V.t.t
     γₜ = γ(t, calibration)
-    ωₜ = ω(t, economy)
+    ωₜ = ω(t, model.economy.abatement)
     r = model.preferences.ρ + Δt⁻¹
 
-    idx = Int[]; jdx = Int[]; values = S[]
+    n = length(G)
+    
+    entrydx = 1
     @inbounds for j in axes(G, 2), i in axes(G, 1)
         k = LinearIndex((i, j), G)
         Xᵢ = G[k]
@@ -27,8 +51,7 @@ function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR,
         y = zero(S) # Diagonal values
         
         # Temperature, which is uncontrolled
-        bᵀ = μ(Xᵢ.T, Xᵢ.m, model) / model.hogg.ϵ
-
+        bᵀ = μ(Xᵢ.T, Xᵢ.m, model.climate) / model.climate.hogg.ϵ
         if (bᵀ ≥ 0 && i < N₁) || (bᵀ < 0 && i == 1)
             ∂ᵀH = (V.H[i + 1, j] - V.H[i, j]) / ΔT₊
 
@@ -42,12 +65,12 @@ function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR,
         end
 
         zdx = min(i + 1, N₁)
-        push!(idx, k); push!(jdx, LinearIndex((zdx, j), G))
-        push!(values, -z)
-        
+        idx[entrydx] = k; jdx[entrydx] = LinearIndex((zdx, j), G); data[entrydx] = -z
+        entrydx += 1
         xdx = max(i - 1, 1)
-        push!(idx, k); push!(jdx, LinearIndex((xdx, j), G))
-        push!(values, -x)
+        idx[entrydx] = k; jdx[entrydx] = LinearIndex((xdx, j), G); data[entrydx] = -x
+        entrydx += 1
+
         y -= (z + x)
 
         # Temperature noise terms (second derivative)
@@ -56,20 +79,20 @@ function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR,
             νT₋ = 2σₜ² / (ΔT₋ * (ΔT₋ + ΔT₊))
             νT₊ = 2σₜ² / (ΔT₊ * (ΔT₋ + ΔT₊))
             
-            push!(idx, k); push!(jdx, LinearIndex((i - 1, j), G))
-            push!(values, -νT₋)
-            push!(idx, k); push!(jdx, LinearIndex((i + 1, j), G))
-            push!(values, -νT₊)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i - 1, j), G); data[entrydx] = -νT₋
+            entrydx += 1
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i + 1, j), G); data[entrydx] = -νT₊
+            entrydx += 1
             y -= (νT₋ + νT₊)
         elseif i == 1
             νT = σₜ² / ΔT₊^2
-            push!(idx, k); push!(jdx, LinearIndex((i + 1, j), G))
-            push!(values, -νT)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i + 1, j), G); data[entrydx] = -νT
+            entrydx += 1
             y -= νT
         elseif i == N₁
             νT = σₜ² / ΔT₋^2
-            push!(idx, k); push!(jdx, LinearIndex((i - 1, j), G))
-            push!(values, -νT)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i - 1, j), G); data[entrydx] = -νT
+            entrydx += 1
             y -= νT
         end
 
@@ -121,12 +144,12 @@ function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR,
         end
 
         zdx = min(j + 1, N₂)
-        push!(idx, k); push!(jdx, LinearIndex((i, zdx), G))
-        push!(values, -z)
+        idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, zdx), G); data[entrydx] = -z
+        entrydx += 1
         
         xdx = max(j - 1, 1)
-        push!(idx, k); push!(jdx, LinearIndex((i, xdx), G))
-        push!(values, -x)
+        idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, xdx), G); data[entrydx] = -x
+        entrydx += 1
         y -= (z + x)
 
         # Carbon concentration noise terms (second derivative)
@@ -135,37 +158,36 @@ function constructA!(V::ValueFunction{S, N₁, N₂}, Δt⁻¹, model::M, G::GR,
             νm₋ = 2σₘ² / (Δm₋ * (Δm₋ + Δm₊))
             νm₊ = 2σₘ² / (Δm₊ * (Δm₋ + Δm₊))
             
-            push!(idx, k); push!(jdx, LinearIndex((i, j - 1), G))
-            push!(values, -νm₋)
-            push!(idx, k); push!(jdx, LinearIndex((i, j + 1), G))
-            push!(values, -νm₊)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, j - 1), G); data[entrydx] = -νm₋
+            entrydx += 1
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, j + 1), G); data[entrydx] = -νm₊
+            entrydx += 1
             y -= (νm₋ + νm₊)
         elseif j == 1
             νm = σₘ² / Δm₊^2
-            push!(idx, k); push!(jdx, LinearIndex((i, j + 1), G))
-            push!(values, -νm)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, j + 1), G); data[entrydx] = -νm
+            entrydx += 1
             y -= νm
         elseif j == N₂
             νm = σₘ² / Δm₋^2
-            push!(idx, k); push!(jdx, LinearIndex((i, j - 1), G))
-            push!(values, -νm)
+            idx[entrydx] = k; jdx[entrydx] = LinearIndex((i, j - 1), G); data[entrydx] = -νm
+            entrydx += 1
             y -= νm
         end
 
-        push!(idx, k); push!(jdx, k)
-        push!(values, r - y)
+        idx[entrydx] = k; jdx[entrydx] = k; data[entrydx] = r - y
+        entrydx += 1
     end
 
-    n = length(G)
-    return sparse(idx, jdx, values, n, n)
+    return SparseArrays.sparse(idx, jdx, data, n, n)
 end
 
 "Constructs source vector `Δt⁻¹ Hⁿ + b`."
-function constructsource(valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitElasticityModel, GR <: AbstractGrid{N₁, N₂, S}}
+function constructsource(valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
     constructsource!(Vector{S}(undef, length(G)), valuefunction, Δt⁻¹, model, G, calibration)
 end
-"Replaces source vector `Δt⁻¹ Hⁿ + b`."
-function constructsource!(source, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitElasticityModel, GR <: AbstractGrid{N₁, N₂, S}}
+"Updates source vector `Δt⁻¹ Hⁿ + b`."
+function constructsource!(source, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
     @inbounds for j in axes(G, 2), i in axes(G, 1)
         k = LinearIndex((i, j), G)
 
@@ -179,11 +201,11 @@ function constructsource!(source, valuefunction::ValueFunction, Δt⁻¹, model:
 end
 
 "Constructs advection coefficient `adv(Hⁿ)`."
-function constructadv(valuefunction::ValueFunction, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitElasticityModel, GR <: AbstractGrid{N₁, N₂, S}}
+function constructadv(valuefunction::ValueFunction, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
     constructadv!(Vector{S}(undef, length(G)), valuefunction, model, G, calibration)
 end
-"Constructs advection coefficient `adv(Hⁿ)`."
-function constructadv!(adv, valuefunction::ValueFunction, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitElasticityModel, GR <: AbstractGrid{N₁, N₂, S}}
+"Updates advection coefficient `adv(Hⁿ)`."
+function constructadv!(adv, valuefunction::ValueFunction, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
     γₜ = γ(valuefunction.t.t, calibration)
 
     @inbounds for j in axes(G, 2), i in axes(G, 1)
@@ -194,8 +216,8 @@ function constructadv!(adv, valuefunction::ValueFunction, model::M, G::GR, calib
 
         (ΔT₋, ΔT₊), (Δm₋, Δm₊) = steps(G, i, j)
 
-        bᵀ = μ(Xᵢ.T, Xᵢ.m, model) / model.hogg.ϵ
-        ∂ᵀH = if (bᵀ ≥ 0 && i < size(G, 1)) || (bᵀ < 0 && i == 1)
+        bᵀ = μ(Xᵢ.T, Xᵢ.m, model.climate) / model.climate.hogg.ϵ
+        ∂ᵀH = if (bᵀ ≥ 0 && i < N₁) || (bᵀ < 0 && i == 1)
             (valuefunction.H[i + 1, j] - valuefunction.H[i, j]) / ΔT₊
         else
            (valuefunction.H[i, j] - valuefunction.H[i - 1, j]) / ΔT₋
@@ -208,13 +230,52 @@ function constructadv!(adv, valuefunction::ValueFunction, model::M, G::GR, calib
            (valuefunction.H[i, j] - valuefunction.H[i, j - 1]) / Δm₋
         end
 
-        adv[k] = (∂ᵀH * model.hogg.σₜ / model.hogg.ϵ)^2 + (∂ᵐH * model.hogg.σₘ)^2
+        adv[k] = (∂ᵀH * model.climate.hogg.σₜ / model.climate.hogg.ϵ)^2 + (∂ᵐH * model.climate.hogg.σₘ)^2
     end
 
     return adv
 end
 
-function centralpolicy!(valuefunction::ValueFunction{S, N₁, N₂}, model::M, G::GR, calibration; withnegative = false) where {N₁, N₂, S, M <: UnitElasticityModel{S}, GR <: AbstractGrid{N₁, N₂, S}}
+"Constructs source vector minus advection `b = Δt⁻¹ Hⁿ + l - adv(Hⁿ)`."
+function constructb(valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
+    constructb!(Vector{S}(undef, length(G)), valuefunction, Δt⁻¹, model, G, calibration)
+end
+"Updates source vector minus advection `b = Δt⁻¹ Hⁿ + l - adv(Hⁿ)`."
+function constructb!(b, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration) where {N₁, N₂, S, M <: UnitIAM, GR <: AbstractGrid{N₁, N₂, S}}
+    t = valuefunction.t.t
+    @inbounds for j in axes(G, 2), i in axes(G, 1)
+        k = LinearIndex((i, j), G)
+        Xᵢ = G[k]
+        αᵢ = valuefunction.α[k]
+        
+        source = l(t, Xᵢ, αᵢ, model, calibration) + Δt⁻¹ * valuefunction.H[k]
+        
+        γₜ = γ(t, calibration)
+        (ΔT₋, ΔT₊), (Δm₋, Δm₊) = steps(G, i, j)
+        
+        bᵀ = μ(Xᵢ.T, Xᵢ.m, model.climate) / model.climate.hogg.ϵ
+        ∂ᵀH = if (bᵀ ≥ 0 && i < size(G, 1)) || (bᵀ < 0 && i == 1)
+            (valuefunction.H[i + 1, j] - valuefunction.H[i, j]) / ΔT₊
+        else
+           (valuefunction.H[i, j] - valuefunction.H[i - 1, j]) / ΔT₋
+        end
+        
+        bᵐ = γₜ - αᵢ
+        ∂ᵐH = if (bᵐ ≥ 0 && j < N₂) || (bᵐ < 0 && j == 1)
+            (valuefunction.H[i, j + 1] - valuefunction.H[i, j]) / Δm₊
+        else
+           (valuefunction.H[i, j] - valuefunction.H[i, j - 1]) / Δm₋
+        end
+        
+        adv = (∂ᵀH * model.climate.hogg.σₜ / model.climate.hogg.ϵ)^2 + (∂ᵐH * model.climate.hogg.σₘ)^2
+        
+        b[k] = source - adv
+    end
+
+    return b
+end
+
+function centralpolicy!(valuefunction::ValueFunction{S, N₁, N₂}, model::M, G::GR, calibration; withnegative = false) where {N₁, N₂, S, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
     @unpack H, α = valuefunction
 
     @inbounds for j in axes(H, 2), i in axes(H, 1)
--- scripts/markov/finitedifference.jl
+++ scripts/markov/finitedifference.jl
@@ -1,23 +1,10 @@
-function picardstep!(problem, source, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration::Calibration; withnegative = false) where {N₁, N₂, S, M <: UnitIAM{S}, GR <: AbstractGrid{N₁,N₂,S}}
-    problem.b .= source - constructadv(valuefunction, model, G,  calibration)
-    problem.A = constructA!(valuefunction, Δt⁻¹, model, G, calibration, withnegative)
-
-    sol = solve!(problem)
-    
-    if !SciMLBase.successful_retcode(sol)
-        throw("Picard step solver failed at time $(valuefunction.t.t)!")
-    end
-
-    return problem
-end
-
 "Takes a step backward in time `H(t) -> H(t - Δt)`."
-function backwardstep!(problem, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration::Calibration; withnegative = false) where {N₁, N₂, S, M <: UnitIAM{S}, GR <: AbstractGrid{N₁,N₂,S}}
+function backwardstep!(problem, stencil, valuefunction::ValueFunction, Δt⁻¹, model::M, G::GR, calibration::Calibration; withnegative = false) where {N₁, N₂, S, M <: UnitIAM{S}, GR <: AbstractGrid{N₁,N₂,S}}
     # Construct the sparse LHS matrix
-    problem.A .= constructA!(valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+    problem.A .= constructA!(stencil, valuefunction, Δt⁻¹, model, G, calibration, withnegative)
 
     # Consutruct the RHS
-    problem.b .= constructsource(valuefunction, Δt⁻¹, model, G, calibration) - constructadv(valuefunction, model, G, calibration)
+    constructb!(problem.b, valuefunction, Δt⁻¹, model, G, calibration)
 
     sol = solve!(problem)
 
@@ -35,38 +22,48 @@ function updateovergrid!(H, u, θ)
 end
 
 "Iterate linear solver until convergence"
-function steadystate!(valuefunction::ValueFunction{S, N₁, N₂}, Δt::S, model::M, G::GR, calibration; timeiterations = 10_000, picarditerations = 100, printstep = 100, tolerance::Error{S} = Error{S}(1e-3, 1e-3), verbose = 0, withnegative = false, alg = KLUFactorization(), θ = 0.9) where {S, N₁, N₂, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
+function steadystate!(valuefunction::ValueFunction{S, N₁, N₂}, Δt::S, model::M, G::GR, calibration; timeiterations = 10_000, picarditerations = 2, printstep = 100, tolerance::Error{S} = Error{S}(1e-3, 1e-3), verbose = 0, withnegative = false, alg = KLUFactorization(), θ = 0.1) where {S, N₁, N₂, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
     Δt⁻¹ = 1 / Δt
-    A₀ = constructA!(valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+    stencil = makestencil(G)
+
     source = constructsource(valuefunction, Δt⁻¹, model, G, calibration)
-    b₀ = source - constructadv(valuefunction, model, G, calibration)
+    adv = constructadv(valuefunction, model, G, calibration)
+    A₀ = constructA!(stencil, valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+    b₀ = source - adv
 
     # Initialise the problem
     problem = LinearSolve.init(LinearProblem(A₀, b₀), alg)
     itererror = abserror(problem.u, valuefunction.H)
     
-    for iter in 1:timeiterations
-        
-        constructsource!(source, valuefunction, Δt⁻¹, model, G, calibration)
-        
-        for k in 1:picarditerations # Stabilise quadratic approximation
-            picardstep!(problem, source, valuefunction, Δt⁻¹, model, G, calibration; withnegative)
-            updateovergrid!(valuefunction.H, problem.u, θ)
+    for iter in 1:timeiterations  
+        constructadv!(adv, valuefunction, model, G, calibration)
+        for _ in 1:picarditerations # Stabilise quadratic approximation
+            constructsource!(source, valuefunction, Δt⁻¹, model, G, calibration)
+            problem.b .= source - adv  # Use fixed quadratic terms
+            
+            # Update only the linear parts of the operator (drift + diffusion)
+            problem.A = constructA!(stencil, valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+            
+            sol = solve!(problem)
+            if !SciMLBase.successful_retcode(sol)
+                throw("Picard step solver failed at time $(valuefunction.t.t)!")
+            end
+            
+            updateovergrid!(valuefunction.H, problem.u, 0.1)
         end
-        
-        backwardstep!(problem, valuefunction, Δt⁻¹, model, G, calibration; withnegative)
+    
+        backwardstep!(problem, stencil, valuefunction, Δt⁻¹, model, G, calibration; withnegative)
 
         itererror = abserror(problem.u, valuefunction.H)
-        hjberror = maximum(abs, problem.A * vec(valuefunction.H) - problem.b)
 
-        updateovergrid!(valuefunction.H, problem.u, 1.)
+        updateovergrid!(valuefunction.H, problem.u, 0.3)
 
         if itererror < tolerance
             return valuefunction, (iter, itererror)
         end
 
         if (verbose > 1) || (verbose > 0 && iter % printstep == 0)
-            @printf "Iteration %d: absolute step = %.2e, relative step = %.2e, HJB error = %.2e\r" iter itererror.absolute itererror.relative hjberror
+            @printf "Iteration %d: absolute step = %.2e, relative step = %.2e\r" iter itererror.absolute itererror.relative
         end
     end
 
@@ -75,34 +72,12 @@ function steadystate!(valuefunction::ValueFunction{S, N₁, N₂}, Δt::S, model
     return valuefunction, (timeiterations, itererror)
 end
 
-function richardsonsteadystate!(V₁::ValueFunction{S, N₁, N₂}, Δt::S, model::M, G₁::GR, calibration; verbose = 0, withnegative = false, iterkwargs...) where {S, N₁, N₂, M <: UnitIAM{S}, GR}
-    G₂ = halfgrid(G₁)
-    V₂ = interpolateovergrid(V₁, G₁, G₂)
-
-    if verbose > 0 println("Solving smaller problem...") end
-    steadystate!(V₂, Δt, model, G₂, calibration; verbose, withnegative, iterkwargs...)
-    V₂ = interpolateovergrid(V₂, G₂, G₁)
-
-    if verbose > 0 println("Solving larger problem...") end
-    V₁.H .= V₂.H
-    V₁.α .= V₂.α
-    steadystate!(V₁, Δt, model, G₁, calibration; verbose, withnegative, iterkwargs...)
-
-    @inbounds for idx in CartesianIndices(G₁)
-        V₁.H[idx] = 2V₁.H[idx] - V₂.H[idx]
-        αmax = upperbound(V₁.t.t, G₁[idx], model, calibration, withnegative)
-        V₁.α[idx] = clamp(2V₁.α[idx] - V₂.α[idx], 0, αmax)
-    end
-
-    return V₁
-end
-
 function backwardsimulation!(
     valuefunction::ValueFunction{S, N₁, N₂}, Δt::S, model::M, G::GR, calibration::Calibration; 
     t₀ = zero(S), withnegative = false,
     verbose = 0, printstep = S(10), 
-    withsave = true, outdir = "data", overwrite = false, 
-    startcache = valuefunction.t.t, cachestep = S(1)) where {S, N₁, N₂, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
+    withsave = true, outdir = "data", overwrite = false, picarditerations = 2, θ = 0.6,
+    startcache = valuefunction.t.t, cachestep = S(1), alg = KLUFactorization()) where {S, N₁, N₂, M <: UnitIAM{S}, GR <: AbstractGrid{N₁, N₂, S}}
     
     if withsave
         cachepath, cachefile = initcachefile(model,G, outdir, withnegative; overwrite)
@@ -116,17 +91,31 @@ function backwardsimulation!(
     end
 
     Δt⁻¹ = 1 / Δt
-    n = length(G)
-    A₀ = constructA!(valuefunction, Δt⁻¹, model, G, calibration, withnegative)
-    b₀ = Vector{S}(undef, n)
-    problem = LinearSolve.init(LinearProblem(A₀, b₀), KLUFactorization())
+    stencil = makestencil(G)
+    source = constructsource(valuefunction, Δt⁻¹, model, G, calibration)
+    adv = constructadv(valuefunction, model, G, calibration)
+    A₀ = constructA!(stencil, valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+    b₀ = source - adv
 
+    problem = LinearSolve.init(LinearProblem(A₀, b₀), alg) # Initialise the problem
+ 
     while t₀ < valuefunction.t.t
-        backwardstep!(problem, valuefunction, Δt⁻¹, model, G, calibration; withnegative)
-        
-        @inbounds for k in eachindex(problem.u)
-            valuefunction.H[k] = problem.u[k]
+        constructadv!(adv, valuefunction, model, G, calibration)
+        for _ in 1:picarditerations
+            constructsource!(source, valuefunction, Δt⁻¹, model, G, calibration)
+            problem.b .= source - adv  # Use fixed quadratic terms
+            problem.A = constructA!(stencil, valuefunction, Δt⁻¹, model, G, calibration, withnegative)
+            
+            sol = solve!(problem)
+            if !SciMLBase.successful_retcode(sol)
+                throw("Picard step solver failed at time $(valuefunction.t.t)!")
+            end
+            
+            updateovergrid!(valuefunction.H, problem.u, θ)
         end
+    
+        backwardstep!(problem, stencil, valuefunction, Δt⁻¹, model, G, calibration; withnegative)
+        updateovergrid!(valuefunction.H, problem.u, 1.)
 
         valuefunction.t.t -= Δt
 
