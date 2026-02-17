a9137aa3f686e5ed042ad22cbd48be89ed987ded
root <root@IanDell.>
2026-02-17 11:00:12 +0000

    🎉 start regret testing

---
+++ scripts/tests/regret.jl
@@ -0,0 +1,26 @@
+using Test, BenchmarkTools, Revise, UnPack
+using Plots, LaTeXStrings
+default(c=:viridis, label=false, dpi=180)
+
+using Model, Grid
+using Base.Threads
+using SciMLBase
+using Statistics
+using StaticArrays, SparseArrays
+using Interpolations, DataStructures
+
+using LinearSolve, LinearAlgebra
+
+using JLD2, UnPack
+using Dates, Printf
+
+includet("../../src/valuefunction.jl")
+includet("../../src/extend/model.jl")
+includet("../../src/extend/grid.jl")
+includet("../../src/extend/valuefunction.jl")
+includet("../utils/saving.jl")
+includet("../utils/simulating.jl")
+includet("../plotting/utils.jl")
+includet("../markov/utils.jl")
+includet("../markov/chain.jl")
+includet("../markov/finitedifference.jl")
