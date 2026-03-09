79cf1956d7368bebffba4356a3b287e91b3bbdee
NoFishLikeIan <andreatitton96@gmail.com>
2026-03-09 13:29:41 +0100

    🦉 regret problem setup

--- scripts/plotting/planner/regret.jl
+++ scripts/plotting/planner/regret.jl
@@ -26,44 +26,20 @@ push!(PGFPlotsX.CUSTOM_PREAMBLE,
     raw"\DeclareSIUnit{\shortoutput}{tr US\mathdollar / y}",
 )
 
-includet("../../../src/valuefunction.jl")
-includet("../../../src/extend/model.jl")
-includet("../../../src/extend/grid.jl")
-includet("../../../src/extend/valuefunction.jl")
-includet("../utils/simulating.jl")
-includet("../../utils/saving.jl")
-includet("../../utils/simulating.jl")
+includet("../utils.jl")
+includet("../../../src/regret.jl")
+includet("../../utils/approximate.jl")
 
-simpath = "data/simulations/simulation-dense"; @assert isdir(simpath)
+
+## Define plotting constants
+datapath = "data"
 
 SAVEFIG = true;
 PLOTPATH = "../job-market-paper/jeem/plots"
 plotpath = joinpath(PLOTPATH, "regret")
 if !isdir(plotpath) mkpath(plotpath) end
 
-function loadsimulation(threshold, discovery, simpath)
-    thresholdkey = replace("T$(Printf.format(Printf.Format("%.2f"), threshold))", "." => ",")
-    discoverykey = replace("D$(Printf.format(Printf.Format("%.2f"), discovery))", "." => ",")
-    outfile = joinpath(simpath, "$(thresholdkey)_$(discoverykey).jld2")
+regretpolicypath = joinpath(datapath, "regret", "policy.jld2")
+JLD2.@load regretpolicypath weights policybasis
+weights = SVector{size(policybasis)}(weights)
 
-    return JLD2.load_object(outfile)
-end
-
-begin # Labels, colors and axis
-    PALETTE = colorschemes[:grays]
-    colors = get(PALETTE, range(0., 1.; length = length(models)), (0., 1.25))
-
-    colorsbymodel = Dict(models .=> colors)
-    Tmin = 0.0; Tmax = 6.0
-    Tspace = range(Tmin, Tmax; length = 101)
-
-    horizon = 2100. - first(calibration.calibrationspan)
-    yearlytime = 0:1:horizon
-    simtspan = (0, horizon)
-
-    temperatureticks = collect.(makedeviationtickz(0, 6; step=1, digits=0))
-
-    m₀ = log(hogg.M₀ / hogg.Mᵖ)
-    T₀ = hogg.T₀
-    X₀ = SVector(T₀, m₀)
-end;
